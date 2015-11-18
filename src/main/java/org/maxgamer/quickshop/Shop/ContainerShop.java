5945dfcf1f7b28de820c2c5252fff0984cb4b28e
KaiKikuchi <kai.kikuchi27@gmail.com>
2015-11-18 18:40:32 +0100

    Update sign on shop load

--- src/main/java/org/maxgamer/quickshop/Shop/ContainerShop.java
+++ src/main/java/org/maxgamer/quickshop/Shop/ContainerShop.java
@@ -610,14 +610,7 @@ public class ContainerShop implements Shop {
 			}
 			DisplayItem disItem = this.getDisplayItem();
 			Location dispLoc = disItem.getDisplayLocation();
-			if (dispLoc.getBlock() != null && dispLoc.getBlock().getType() == Material.WATER) { // Flowing
-												// water.
-												// Stationery
-												// water
-												// does
-												// not
-												// move
-												// items.
+			if (dispLoc.getBlock() != null && dispLoc.getBlock().getType() == Material.WATER) {
 				disItem.remove();
 				return;
 			}
@@ -645,6 +638,9 @@ public class ContainerShop implements Shop {
 
 	public void onLoad() {
 		checkDisplay();
+		
+		// update sign
+		this.setSignText();
 	}
 
 	public void onClick() {
