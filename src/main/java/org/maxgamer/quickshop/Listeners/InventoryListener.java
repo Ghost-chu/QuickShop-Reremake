1c681b1c6c885daaa6a8c7ea19767b8fb74eeb80
Kai <kai.kikuchi27@gmail.com>
2017-06-05 22:01:42 +0200

    Auto-refresh shop sign when the shop chest interface is closed

--- src/main/java/org/maxgamer/quickshop/Listeners/InventoryListener.java
+++ src/main/java/org/maxgamer/quickshop/Listeners/InventoryListener.java
@@ -1,15 +1,19 @@
 package org.maxgamer.quickshop.Listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.maxgamer.quickshop.QuickShop;
+import org.maxgamer.quickshop.Shop.Shop;
 import org.maxgamer.quickshop.Util.Util;
 
 public class InventoryListener implements Listener {
@@ -33,6 +37,26 @@ public class InventoryListener implements Listener {
 				}
 			} catch (Exception e) {}
 		}
+	}
+	
+	@EventHandler
+	public void onInventoryClose(InventoryCloseEvent event) {
+		final Inventory inventory = event.getInventory();
+		if (inventory == null) {
+			return;
+		}
+		
+		final Location location = inventory.getLocation();
+		if (location == null) {
+			return;
+		}
+		
+		final Shop shop = plugin.getShopManager().getShop(location);
+		if (shop == null) {
+			return;
+		}
+		
+		shop.setSignText();
 	}
 
 	@EventHandler
