b68984b1764ce1bae7559bf14260909dd4dc5bee
Ghost_chu <2908803755@qq.com>
2018-07-31 17:35:53 +0800

    Cleanup code

--- src/main/java/org/maxgamer/quickshop/Listeners/PlayerListener.java
+++ src/main/java/org/maxgamer/quickshop/Listeners/PlayerListener.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
