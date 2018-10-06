8a1caa9ab3ad6ca9c62ea14622e73544caa55cc1
Ghost_chu <2908803755@qq.com>
2018-10-06 15:45:43 +0800

    Change bstats from QuickShop to QuickShop-Reremake
    Former-commit-id: 766b97fa3b3ae6fdd15e69105a0a6507207e5326

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -31,7 +31,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
