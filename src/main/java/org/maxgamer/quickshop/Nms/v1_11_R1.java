a89b263f4ab331c003e9ae39b91eee1929beea32
Ghost_chu <2908803755@qq.com>
2018-10-29 17:13:51 +0800

    Finish legacy version support, but may have issues, nms may import wrong(I remeber 1.12.2 should is V1_12_R2 but only have R1), need more test
    Former-commit-id: d2708beb6d07bf8484085e5b03c6cea17784c6ab

---
+++ src/main/java/org/maxgamer/quickshop/Nms/v1_11_R1.java
@@ -0,0 +1,19 @@
+package org.maxgamer.quickshop.Nms;
+
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_11_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_11_R1.NBTTagCompound;
+
+public class v1_11_R1 {
+    public String getNBTJson(ItemStack iStack) {
+        if(iStack.getType().equals(Material.AIR)) {
+            return null;
+        }
+        net.minecraft.server.v1_11_R1.ItemStack mcitem = CraftItemStack.asNMSCopy(iStack);
+        NBTTagCompound nbt = new NBTTagCompound();
+        mcitem.save(nbt);
+        return nbt.toString();
+    }
+}
