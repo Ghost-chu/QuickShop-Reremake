f0fd13ea753b6df7da28189358ec4d917f96dad5
Sotr <i@omc.hk>
2018-10-29 22:30:46 +0800

    Fixes air material
    Former-commit-id: 5fecaa9300aa15a2c586da00fdb19f2d9d60ce71

--- src/main/java/org/maxgamer/quickshop/Util/ItemNMS.java
+++ src/main/java/org/maxgamer/quickshop/Util/ItemNMS.java
@@ -1,21 +1,20 @@
 package org.maxgamer.quickshop.Util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.maxgamer.quickshop.QuickShop;
 
 import java.lang.reflect.Method;
 
-public abstract class ItemNMS {
-    private static final String nmsVersion;
-    
+public abstract class ItemNMS { 
     private static Method craftItemStack_asNMSCopyMethod;
     private static Class<?> nbtTagCompoundClass;
     private static Method itemStack_saveMethod;
     
     static {
         String name = Bukkit.getServer().getClass().getPackage().getName();
-        nmsVersion = name.substring(name.lastIndexOf('.') + 1);
+        String nmsVersion = name.substring(name.lastIndexOf('.') + 1);
         
         try {
             craftItemStack_asNMSCopyMethod = Class.forName("org.bukkit.craftbukkit." + nmsVersion + ".inventory.CraftItemStack")
@@ -34,6 +33,8 @@ public abstract class ItemNMS {
     }
 	
 	public static String saveJsonfromNMS(ItemStack bStack) throws Throwable {
+	    if (bStack.getType() == Material.AIR)
+	        return null;
         Object mcStack = craftItemStack_asNMSCopyMethod.invoke(null, bStack);
         Object nbtTagCompound = nbtTagCompoundClass.newInstance();
         
