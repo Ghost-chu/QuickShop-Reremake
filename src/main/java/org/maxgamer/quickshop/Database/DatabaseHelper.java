25f583a03a318d81eaa98714967f6ab3fe04e96b
Ghost_chu <2908803755@qq.com>
2018-12-04 14:45:21 +0800

    Useless reconnect check, cause Java will auto-reconnect
    Former-commit-id: 8cc49511c9388e3fd3b57b36a37153daf466d6f1

--- src/main/java/org/maxgamer/quickshop/Database/DatabaseHelper.java
+++ src/main/java/org/maxgamer/quickshop/Database/DatabaseHelper.java
@@ -12,7 +12,6 @@ import org.maxgamer.quickshop.Util.Util;
 
 public class DatabaseHelper {
 	public static void setup(Database db) throws SQLException {
-		connectCheck();
 		if (!db.hasTable(QuickShop.instance.dbPrefix+"shops")) {
 			createShopsTable(db);
 		}
@@ -26,7 +25,6 @@ public class DatabaseHelper {
 	 * Verifies that all required columns exist.
 	 */
 	public static void checkColumns(Database db) {
-		connectCheck();
 		PreparedStatement ps = null;
 		try {
 			// V3.4.2
@@ -51,7 +49,6 @@ public class DatabaseHelper {
 	 *             If the connection is invalid.
 	 */
 	public static void createShopsTable(Database db) throws SQLException {
-		connectCheck();
 		Statement st = db.getConnection().createStatement();
 		String createTable = null;
 		if(QuickShop.instance.getConfig().getBoolean("database.use-varchar")) {
@@ -69,7 +66,6 @@ public class DatabaseHelper {
 	 *             If the connection is invalid
 	 */
 	public static void createMessagesTable(Database db) throws SQLException {
-		connectCheck();
 		Statement st = db.getConnection().createStatement();
 		String createTable = null;
 		if(QuickShop.instance.getConfig().getBoolean("database.use-varchar")) {
@@ -82,27 +78,23 @@ public class DatabaseHelper {
 	}
 	
 	public static ResultSet selectAllShops(Database db) throws SQLException {
-		connectCheck();
 		PreparedStatement ps =  db.getConnection().prepareStatement("SELECT * FROM "+QuickShop.instance.dbPrefix+"shops");
 		return ps.executeQuery();
 		
 	}
 	public static ResultSet selectAllMessages(Database db) throws SQLException {
-		connectCheck();
 		PreparedStatement ps =  db.getConnection().prepareStatement("SELECT * FROM "+QuickShop.instance.dbPrefix+"messages");
 		return ps.executeQuery();
 		
 	}
 
 	public static void removeShop(Database db, int x, int y, int z, String worldName) throws SQLException {
-		connectCheck();
 		db.getConnection().createStatement()
 				.executeUpdate("DELETE FROM " + QuickShop.instance.dbPrefix + "shops WHERE x = " + x + " AND y = " + y
 						+ " AND z = " + z + " AND world = \"" + worldName + "\""
 						+ (db.getCore() instanceof MySQLCore ? " LIMIT 1" : ""));
 	}
 	public static void updateOwner2UUID(String ownerUUID, int x, int y, int z, String worldName) throws SQLException {
-		connectCheck();
 		QuickShop.instance.getDB().getConnection().createStatement()
 		.executeUpdate("UPDATE "+QuickShop.instance.dbPrefix+"shops SET owner = \"" + ownerUUID.toString()
 		+ "\" WHERE x = " + x + " AND y = " + y + " AND z = " + z
@@ -112,13 +104,11 @@ public class DatabaseHelper {
 
 	public static void updateShop(Database db, String owner, ItemStack item, int unlimited, int shopType,
 			double price, int x, int y, int z, String world) {
-		connectCheck();
 		String q = "UPDATE "+QuickShop.instance.dbPrefix+"shops SET owner = ?, itemConfig = ?, unlimited = ?, type = ?, price = ? WHERE x = ? AND y = ? and z = ? and world = ?";
 		db.execute(q, owner, Util.serialize(item), unlimited, shopType, price, x, y, z, world);
 	}
 	
 	public static void createShop(String owner, double price, ItemStack item, int unlimited, int shopType, String world, int x, int y, int z) {
-		connectCheck();
 		String q = "INSERT INTO "+QuickShop.instance.dbPrefix+"shops (owner, price, itemConfig, x, y, z, world, unlimited, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		QuickShop.instance.getDB().execute(q, owner, price, Util.serialize(item), x, y, z, world, unlimited, shopType);
 	}
@@ -129,45 +119,6 @@ public class DatabaseHelper {
 	
 	public static void cleanMessage(long weekAgo) {
 		QuickShop.instance.getDB().execute("DELETE FROM "+QuickShop.instance.dbPrefix+"messages WHERE time < ?", weekAgo);
-	}
-	
-	public static void connectCheck(){
-		if(!QuickShop.instance.getConfig().getBoolean("database.reconnect")) {
-			return;
-		}
-		try {
-			if(QuickShop.instance.getDB().getConnection().isClosed())
-			QuickShop.instance.getLogger().severe("Database connection lost, Reconnecting...");
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			if(QuickShop.instance.getDB().getConnection().isReadOnly()) {
-			QuickShop.instance.getLogger().severe("Database is read-only, QuickShop can't write in data!");
-			return;
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			try {
-				if (!QuickShop.instance.getDB().getConnection().isValid(10)) {
-					boolean result = QuickShop.instance.setupDatabase(); // Reconnect
-					if(result) {
-						//Failed reconnect.
-						QuickShop.instance.getLogger().severe("Failed to reconnect.");
-					}else {
-						QuickShop.instance.getLogger().severe("Reconnected.");
-						connectCheck(); //Recall to check
-					}
-				}
-			} catch (AbstractMethodError e) {
-				// You don't need to validate this core.
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
 	}
 	
 }
