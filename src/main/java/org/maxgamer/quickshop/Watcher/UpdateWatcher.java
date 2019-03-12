e7a78b21ec31554f39860e9cedce32341533ab59
Ghost_chu <2908803755@qq.com>
2019-03-12 15:16:10 +0800

    Updater update
    Former-commit-id: b09770d5e9a18745f9fbc95507819e6d212cfb1f [formerly ea4158c7845c3780e2c8dc9c2c71b2eeb9f0fd56]
    Former-commit-id: 3099631bbdc7b3969153951f24ec72a2230f8014

--- src/main/java/org/maxgamer/quickshop/Watcher/UpdateWatcher.java
+++ src/main/java/org/maxgamer/quickshop/Watcher/UpdateWatcher.java
@@ -7,24 +7,39 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.maxgamer.quickshop.QuickShop;
+import org.maxgamer.quickshop.Util.UpdateInfomation;
 import org.maxgamer.quickshop.Util.Updater;
 
 public class UpdateWatcher implements Listener {
 	static BukkitTask cronTask = null;
 	static boolean hasNewUpdate = false;
+	static boolean isBeta = false;
+	static UpdateInfomation info = null;
 
 	public static void init() {
 		cronTask = new BukkitRunnable() {
 
 			@Override
 			public void run() {
-				if (!Updater.checkUpdate()) {
+				info = Updater.checkUpdate();
+				if (!info.getIsNewUpdate()) {
 					hasNewUpdate = false;
 					return;
 				}
-				QuickShop.instance.getLogger().info("New QuickShop released, now updated on SpigotMC.org!");
-				QuickShop.instance.getLogger().info("Update here: https://www.spigotmc.org/resources/62575/");
-				hasNewUpdate = true;
+				
+				if(!info.getIsBeta()) {
+					QuickShop.instance.getLogger().info("New QuickShop released, now updated on SpigotMC.org!");
+					QuickShop.instance.getLogger().info("Update here: https://www.spigotmc.org/resources/62575/");
+					hasNewUpdate = true;
+				}else {
+					QuickShop.instance.getLogger().info("New QuickShop BETA released, you can updated on SpigotMC.org!");
+					QuickShop.instance.getLogger().info("Update here: https://www.spigotmc.org/resources/62575/");
+					QuickShop.instance.getLogger().info("This is a BETA version, that mean you shouldn't use it in Production Environment");
+					hasNewUpdate = true;
+				}
+				
+				
+				
 			}
 		}.runTaskTimerAsynchronously(QuickShop.instance, 1, 20 * 60 * 60);
 	}
@@ -43,9 +58,14 @@ public class UpdateWatcher implements Listener {
 			@Override
 			public void run() {
 				if (e.getPlayer().hasPermission("quickshop.alert") && hasNewUpdate) {
-					e.getPlayer().sendMessage(ChatColor.GREEN + "New QuickShop released, now updated on SpigotMC.org!");
-					e.getPlayer().sendMessage(
-							ChatColor.GREEN + "Update here: https://www.spigotmc.org/resources/62575/");
+					if(!info.getIsBeta()) {
+						e.getPlayer().sendMessage(ChatColor.GREEN+"New QuickShop released, now updated on SpigotMC.org!");
+						e.getPlayer().sendMessage(ChatColor.GREEN+"Update here: https://www.spigotmc.org/resources/62575/");
+					}else {
+						e.getPlayer().sendMessage(ChatColor.GRAY+"New QuickShop BETA released, you can updated on SpigotMC.org!");
+						e.getPlayer().sendMessage(ChatColor.GRAY+"Update here: https://www.spigotmc.org/resources/62575/");
+						e.getPlayer().sendMessage(ChatColor.GRAY+"This is a BETA version, that mean you shouldn't use it in Production Environment");
+					}
 				}
 
 			}
