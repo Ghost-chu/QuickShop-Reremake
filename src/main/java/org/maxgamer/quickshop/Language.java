16bf5d098d96b041c820e97faaaa614aaf5eb2bc
Ghost_chu <2908803755@qq.com>
2019-04-05 22:17:07 +0800

    Add i18n
    Former-commit-id: f6641a9f69f41fd05b6d87fc4e0cd435b0a38a5f

---
+++ src/main/java/org/maxgamer/quickshop/Language.java
@@ -0,0 +1,27 @@
+package org.maxgamer.quickshop;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Language {
+	QuickShop plugin;
+	ArrayList<String> languages = new ArrayList<>();
+	public Language(QuickShop plugin) {
+		this.plugin = plugin;
+		languages.clear();
+		languages.add("en_US");
+	}
+	public String getComputerLanguage() {
+		return Locale.getDefault().toString();
+	}
+	public ArrayList<String> getSupportsLanguageList() {
+		return languages;
+	}
+	public InputStream getFile(String language, String type) {
+		if(language==null || !languages.contains(language))
+			language="en_US";
+		return plugin.getResource(type+"-"+language+".yml");
+		//File name should call    type-language.yml    ---> config-zh_CN.yml
+	}
+}
