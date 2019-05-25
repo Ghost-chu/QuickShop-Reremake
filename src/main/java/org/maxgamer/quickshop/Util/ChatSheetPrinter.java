ba4a270d20af146be72b6d69a6cd828586339be3
Ghost_chu <2908803755@qq.com>
2019-05-25 22:01:01 +0800

    Switch to use ChatSheetPrinter
    Former-commit-id: e292e4a5892d75c56fd4038ab28b3b69d64e1014 [formerly b750ea20458b64fbc91838a0b92c0811533185c3]
    Former-commit-id: 6a60d42336b1f02a509d08e374d046930272a71e

---
+++ src/main/java/org/maxgamer/quickshop/Util/ChatSheetPrinter.java
@@ -0,0 +1,50 @@
+package org.maxgamer.quickshop.Util;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class ChatSheetPrinter {
+    //StringBuffer buffer = new StringBuffer();
+    CommandSender p;
+    ChatColor chatColor = ChatColor.DARK_PURPLE;
+    public ChatSheetPrinter(CommandSender sender){
+        this.p = sender;
+    }
+
+    public void printHeader(){
+        p.sendMessage("");
+        p.sendMessage("");
+        p.sendMessage(chatColor + MsgUtil.getMessage("tableformat.full_line"));
+    }
+
+    public void printLine(String text){
+        p.sendMessage(chatColor + MsgUtil.getMessage("tableformat.left_begin")+" "+text);
+    }
+
+    public void printCenterLine(String text){
+        p.sendMessage(chatColor + MsgUtil.getMessage("tableformat.left_half_line")+text+MsgUtil.getMessage("tableformat.right_half_line"));
+    }
+
+    public void printFooter(){
+        p.sendMessage(chatColor + MsgUtil.getMessage("tableformat.full_line"));
+    }
+
+    public void printExecuteableCmdLine(String text, String hoverText, String executeCmd){
+        TextComponent message = new TextComponent(chatColor + MsgUtil.getMessage("tableformat.left_begin") + text);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, executeCmd));
+        message.setHoverEvent(
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hoverText).create()));
+        p.spigot().sendMessage(message);
+    }
+    public void printSuggestableCmdLine(String text, String hoverText, String suggestCmd){
+        TextComponent message = new TextComponent(chatColor + MsgUtil.getMessage("tableformat.left_begin") + text);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, suggestCmd));
+        message.setHoverEvent(
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hoverText).create()));
+        p.spigot().sendMessage(message);
+    }
+}
