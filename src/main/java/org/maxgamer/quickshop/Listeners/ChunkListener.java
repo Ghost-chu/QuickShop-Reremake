5d017a7595cc05fbfeff5027f61e128e5423a720
Ghost_chu <2908803755@qq.com>
2019-05-27 22:10:57 +0800

    Format code by IDEA IDE
    Former-commit-id: 0d59111dd53cde3a9546f42c76a754b6b9c0269e

--- src/main/java/org/maxgamer/quickshop/Listeners/ChunkListener.java
+++ src/main/java/org/maxgamer/quickshop/Listeners/ChunkListener.java
@@ -12,43 +12,45 @@ import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.Shop.Shop;
+import org.maxgamer.quickshop.Util.Util;
 
 public class ChunkListener implements Listener {
-	private QuickShop plugin;
+    private QuickShop plugin;
 
-	public ChunkListener(QuickShop plugin) {
-		this.plugin = plugin;
-	}
+    public ChunkListener(QuickShop plugin) {
+        this.plugin = plugin;
+    }
 
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-	public void onChunkLoad(ChunkLoadEvent e) {
-		Chunk c = e.getChunk();
-		if (plugin.getShopManager().getShops() == null)
-			return;
-		HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
-		if (inChunk == null)
-			return;
-		new BukkitRunnable() {
-			
-			@Override
-			public void run() {
-				for (Shop shop : inChunk.values()) {
-					shop.onLoad();
-					shop.setSignText();
-				}
-				
-			}
-		}.runTaskLater(plugin, 1); //Delay 1 tick, hope can fix the magic bug in 1.14 spigot build.
-	}
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onChunkLoad(ChunkLoadEvent e) {
+        Chunk c = e.getChunk();
+        if (plugin.getShopManager().getShops() == null)
+            return;
+        HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
+        if (inChunk == null)
+            return;
+        new BukkitRunnable() {
 
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onChunkUnload(ChunkUnloadEvent e) {
-		Chunk c = e.getChunk();
-		HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
-		if (inChunk == null)
-			return;
-		for (Shop shop : inChunk.values()) {
-			shop.onUnload();
-		}
-	}
+            @Override
+            public void run() {
+                for (Shop shop : inChunk.values()) {
+                    Util.debugLog("Loading shop at " + shop.getLocation().toString());
+                    shop.onLoad();
+                    shop.setSignText();
+                }
+
+            }
+        }.runTaskLater(plugin, 1); //Delay 1 tick, hope can fix the magic bug in 1.14 spigot build.
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChunkUnload(ChunkUnloadEvent e) {
+        Chunk c = e.getChunk();
+        HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
+        if (inChunk == null)
+            return;
+        for (Shop shop : inChunk.values()) {
+            shop.onUnload();
+        }
+    }
 }
--- src/main/java/org/maxgamer/quickshop/Shop/ShopCreateEvent.java
+++ src/main/java/org/maxgamer/quickshop/Shop/ShopCreateEvent.java
@@ -4,53 +4,52 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.maxgamer.quickshop.Shop.Shop;
 
 public class ShopCreateEvent extends Event implements Cancellable {
-	private static final HandlerList handlers = new HandlerList();
-	private Shop shop;
-	private boolean cancelled;
-	private Player p;
+    private static final HandlerList handlers = new HandlerList();
+    private Shop shop;
+    private boolean cancelled;
+    private Player p;
 
-	public ShopCreateEvent(Shop shop, Player p) {
-		this.shop = shop;
-		this.p = p;
-	}
+    public ShopCreateEvent(Shop shop, Player p) {
+        this.shop = shop;
+        this.p = p;
+    }
 
-	/**
-	 * The shop to be created
-	 * 
-	 * @return The shop to be created
-	 */
-	public Shop getShop() {
-		return this.shop;
-	}
+    /**
+     * The shop to be created
+     *
+     * @return The shop to be created
+     */
+    public Shop getShop() {
+        return this.shop;
+    }
 
-	/**
-	 * The player who is creating this shop
-	 * 
-	 * @return The player who is creating this shop
-	 */
-	public Player getPlayer() {
-		return p;
-	}
+    /**
+     * The player who is creating this shop
+     *
+     * @return The player who is creating this shop
+     */
+    public Player getPlayer() {
+        return p;
+    }
 
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
-	}
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
-	public static HandlerList getHandlerList() {
-		return handlers;
-	}
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
-	@Override
-	public boolean isCancelled() {
-		return this.cancelled;
-	}
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
 
-	@Override
-	public void setCancelled(boolean cancel) {
-		this.cancelled = cancel;
-	}
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
 }
