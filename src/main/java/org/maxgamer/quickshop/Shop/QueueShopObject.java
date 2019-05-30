0e7af5784f6553b9e1eac5aee15172f3dd4d2448
Ghost_chu <2908803755@qq.com>
2019-05-30 14:41:21 +0800

    Improve default config and add javadoc
    Former-commit-id: 7dd362e437602dbbe9a17a9f8613445b53de63e3 [formerly 4e75a17cea9e5597ab1495b71298510c5b3b0bea]
    Former-commit-id: 0e27a944482c182e1dba070869b414bbf5bc876b

--- src/main/java/org/maxgamer/quickshop/Shop/QueueShopObject.java
+++ src/main/java/org/maxgamer/quickshop/Shop/QueueShopObject.java
@@ -6,6 +6,12 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 public class QueueShopObject {
+    /**
+     * The shop you want to do actions
+     */
     private Shop shop;
+    /**
+     * The actions you want to do
+     */
     private QueueAction[] action;
 }
