7363d1a103501a645deb03ef4e23a4f4c34c6923
Ghost_chu <2908803755@qq.com>
2019-06-02 22:09:37 +0800

    Update javadoc

--- src/main/java/org/maxgamer/quickshop/Util/Compatibility.java
+++ src/main/java/org/maxgamer/quickshop/Util/Compatibility.java
@@ -20,6 +20,7 @@ public class Compatibility {
     /**
      * Switch the compatibility mode on or off, set false to disable all we known incompatiable plugin listener,
      * set true to enable back all disabled plugin liseners.
+     * WIP
      *
      * @param status true=turn on closed listeners, false=turn off all turned on listeners.
      */
