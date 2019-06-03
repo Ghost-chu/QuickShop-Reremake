707b6774d8c5f50bb4336e82271a456bfbed4a80
Ghost_chu <2908803755@qq.com>
2019-06-03 16:54:39 +0800

    Some performance improve
    Former-commit-id: 5d81d47b2b168c31d53c1938993540944d3e332b [formerly 3153ae482aac09f24f1cab0b869e4be83add7b02]
    Former-commit-id: 6a103f732666eeecfb86de19f5eca00e1843e311

--- src/main/java/org/maxgamer/quickshop/Listeners/ChunkListener.java
+++ src/main/java/org/maxgamer/quickshop/Listeners/ChunkListener.java
@@ -1,7 +1,6 @@
 package org.maxgamer.quickshop.Listeners;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import lombok.*;
 import org.bukkit.Chunk;
@@ -16,19 +15,15 @@ import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.Shop.QueueAction;
 import org.maxgamer.quickshop.Shop.QueueShopObject;
 import org.maxgamer.quickshop.Shop.Shop;
-import org.maxgamer.quickshop.Shop.ShopChunk;
 import org.maxgamer.quickshop.Util.Util;
 
 @AllArgsConstructor
 public class ChunkListener implements Listener {
     private QuickShop plugin;
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onChunkLoad(ChunkLoadEvent e) {
         Chunk c = e.getChunk();
-        Map<String, HashMap<ShopChunk, HashMap<Location, Shop>>> map = plugin.getShopManager().getShops();
-        if (plugin.getShopManager().getShops() == null || map.isEmpty())
-            return;
         HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
         if (inChunk == null || inChunk.isEmpty())
             return;
@@ -46,7 +41,7 @@ public class ChunkListener implements Listener {
         }.runTaskLater(plugin, 1); //Delay 1 tick, hope can fix the magic bug in 1.14 spigot build.
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onChunkUnload(ChunkUnloadEvent e) {
         Chunk c = e.getChunk();
         HashMap<Location, Shop> inChunk = plugin.getShopManager().getShops(c);
