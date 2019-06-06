46fce3632932af583b51939aea2d890b1d93d02e
Ghost_chu <2908803755@qq.com>
2019-06-06 18:52:10 +0800

    Sync code with Vlxingze
    Former-commit-id: 94053af283c9f2bbdaa56e94ca1e4f10105588e9

--- src/main/java/org/maxgamer/quickshop/Shop/DisplayItem.java
+++ src/main/java/org/maxgamer/quickshop/Shop/DisplayItem.java
@@ -239,6 +239,7 @@ public interface DisplayItem {
 
     public static DisplayType getNowUsing() {
         //TODO Read config and choosen which type we need to use.
+        return DisplayType.fromID(QuickShop.instance.getConfig().getInt("shop.display-type"));
     }
 
 }
