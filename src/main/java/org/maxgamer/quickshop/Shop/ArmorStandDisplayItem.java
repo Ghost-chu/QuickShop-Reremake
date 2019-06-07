72eb5e20bceb6555b37f6959ca2957e03e6eb973
Vlele <1207223090@qq.com>
2019-06-07 17:59:44 +0800

    Dix again:ArmorStandDisplay something wrong
    Former-commit-id: 98eb6c065460d8235c2318457e18f816f9693378 [formerly 23e50db48d2e32eb67b832f467edd54422c7b758]
    Former-commit-id: 992063bac18c04acc88396267f8e16fb1f44b054

--- src/main/java/org/maxgamer/quickshop/Shop/ArmorStandDisplayItem.java
+++ src/main/java/org/maxgamer/quickshop/Shop/ArmorStandDisplayItem.java
@@ -66,7 +66,7 @@ public class ArmorStandDisplayItem implements DisplayItem {
         //this.armorStand.setSmall(true);
 
         //Set armorstand item in hand
-        this.armorStand.setItemInHand(originalItemStack.clone());
+        this.armorStand.setItemInHand(new ItemStack(originalItemStack.getType()));
         //Set safeGuard
         safeGuard(this.armorStand);
         //Set pose
@@ -82,8 +82,8 @@ public class ArmorStandDisplayItem implements DisplayItem {
         }
         ArmorStand armorStand = (ArmorStand) entity;
         //Set item protect in the armorstand's hand
-        this.guardedIstack = DisplayItem.createGuardItemStack(this.originalItemStack);
-        armorStand.setItemInHand(guardedIstack.clone());
+//        this.guardedIstack = DisplayItem.createGuardItemStack(this.originalItemStack);
+        armorStand.setItemInHand(new ItemStack(originalItemStack.getType()));
         Util.debugLog("Successfully safeGuard ArmorStand: " + armorStand.getLocation().toString());
     }
 
