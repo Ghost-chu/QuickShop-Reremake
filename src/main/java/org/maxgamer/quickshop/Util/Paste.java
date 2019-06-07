6419d15681257e6e3e274127ff085a89a0706756
Ghost_chu <2908803755@qq.com>
2019-06-07 21:55:56 +0800

    Catch the possible exception in paste
    Former-commit-id: 261ce190781c14e0d5a01b311cdf9aad97d71007

--- src/main/java/org/maxgamer/quickshop/Util/Paste.java
+++ src/main/java/org/maxgamer/quickshop/Util/Paste.java
@@ -80,20 +80,24 @@ public class Paste {
         }
         finalReport.append("================================================\n");
         finalReport.append("Configurations:\n");
-        finalReport.append("\t*********************************\n");
-        finalReport.append("\tconfig.yml:\n");
-        finalReport.append("\t\t\n" + new String(Util.inputStream2ByteArray(Bukkit.getPluginManager().getPlugin(plugin.getName())
-                .getDataFolder().toString() + "/config.yml")) + "\n");
-        finalReport.append("\t*********************************\n");
-        finalReport.append("\tmessages.yml:\n");
-        finalReport.append("\t\t\n" + new String(Util.inputStream2ByteArray(Bukkit.getPluginManager().getPlugin(plugin.getName())
-                .getDataFolder().toString() + "/messages.yml")) + "\n");
-        finalReport.append("\t*********************************\n");
-        finalReport.append("\t*********************************\n");
-        finalReport.append("\tlatest.log:\n");
-        finalReport.append("\t\t\n" + new String(Util
-                .inputStream2ByteArray(new File(new File(".", "logs"), "latest.log").getPath())) + "\n");
-        finalReport.append("\t*********************************\n");
+        try {
+            finalReport.append("\t*********************************\n");
+            finalReport.append("\tconfig.yml:\n");
+            finalReport.append("\t\t\n" + new String(Util
+                    .inputStream2ByteArray(plugin.getDataFolder().toString() + "/config.yml")) + "\n");
+            finalReport.append("\t*********************************\n");
+            finalReport.append("\tmessages.yml:\n");
+            finalReport.append("\t\t\n" + new String(Util
+                    .inputStream2ByteArray(plugin.getDataFolder().toString() + "/messages.yml")) + "\n");
+            finalReport.append("\t*********************************\n");
+            finalReport.append("\t*********************************\n");
+            finalReport.append("\tlatest.log:\n");
+            finalReport.append("\t\t\n" + new String(Util
+                    .inputStream2ByteArray(new File(new File(".", "logs"), "latest.log").getPath())) + "\n");
+            finalReport.append("\t*********************************\n");
+        } catch (Throwable th) {
+            finalReport.append("\tFailed to get data\n");
+        }
         finalReport.append("================================================\n");
         return finalReport.toString();
     }
