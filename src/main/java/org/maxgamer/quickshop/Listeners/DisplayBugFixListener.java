6227cef55f39b3871d3c91f61e01bcbe6e2921ad
Ghost_chu <2908803755@qq.com>
2019-06-08 18:41:39 +0800

    Allow player place block when displayItem ArmorStand in target loc.
    Former-commit-id: dcfcc9b2e3da5bbc6e1525e515be376d414f739e [formerly 71962611977c49ae7e570e82c98be1fd114d11da]
    Former-commit-id: 175dbd4e831b2bdfe102a6302138ee207c92bd13

---
+++ src/main/java/org/maxgamer/quickshop/Listeners/DisplayBugFixListener.java
@@ -0,0 +1,43 @@
+package org.maxgamer.quickshop.Listeners;
+
+import java.util.Collection;
+
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.maxgamer.quickshop.QuickShop;
+import org.maxgamer.quickshop.Shop.DisplayItem;
+import org.maxgamer.quickshop.Shop.DisplayType;
+import org.maxgamer.quickshop.Util.Util;
+
+public class DisplayBugFixListener implements Listener {
+    private QuickShop plugin;
+
+    public DisplayBugFixListener(QuickShop plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void canBuild(BlockCanBuildEvent e) {
+        if (!plugin.isDisplay())
+            return;
+        if (DisplayItem.getNowUsing() != DisplayType.ARMORSTAND)
+            return;
+        if (e.isBuildable())
+            return;
+        Collection<Entity> entities = e.getBlock().getWorld().getNearbyEntities(e.getBlock().getLocation(), 1.0, 1, 1.0);
+        for (Entity entity : entities) {
+            Util.debugLog("Found entity: " + entity.getType().name() + " at " + entity.getLocation());
+            if (!(entity instanceof ArmorStand))
+                continue;
+            ArmorStand armorStand = (ArmorStand) entity;
+            if (DisplayItem.checkIsGuardItemStack(armorStand.getItemInHand())) {
+                e.setBuildable(true);
+                Util.debugLog("Re-set the allow build here cause found cause display-item blocking.");
+                break;
+            }
+        }
+    }
+}
