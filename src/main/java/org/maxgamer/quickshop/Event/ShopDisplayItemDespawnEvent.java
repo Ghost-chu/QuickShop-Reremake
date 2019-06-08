63e1aa1a1fd8e80562e370aa0fcad54316e7c7e4
Ghost_chu <2908803755@qq.com>
2019-06-08 16:56:16 +0800

    A lot new event
    Former-commit-id: 0f9c9055b6ec769b6c2d83abc5b37d38367df37b

---
+++ src/main/java/org/maxgamer/quickshop/Event/ShopDisplayItemDespawnEvent.java
@@ -0,0 +1,70 @@
+package org.maxgamer.quickshop.Event;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+import org.maxgamer.quickshop.Shop.DisplayItem;
+import org.maxgamer.quickshop.Shop.DisplayType;
+import org.maxgamer.quickshop.Shop.Shop;
+
+/**
+ * This event is called after DisplayItem removed
+ */
+public class ShopDisplayItemDespawnEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    private Shop shop;
+    private ItemStack iStack;
+    private DisplayType displayType;
+
+    /*
+     * This event is called before the shop display item created
+     */
+    public ShopDisplayItemDespawnEvent(Shop shop, ItemStack iStack) {
+        this.shop = shop;
+        this.iStack = iStack;
+        this.displayType = DisplayItem.getNowUsing();
+    }
+
+    /*
+     * This event is called before the shop display item created
+     */
+    public ShopDisplayItemDespawnEvent(Shop shop, ItemStack iStack, DisplayType displayType) {
+        this.shop = shop;
+        this.iStack = iStack;
+        this.displayType = displayType;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public DisplayType getDisplayType() {
+        return displayType;
+    }
+
+    public ItemStack getItemStack() {
+        return iStack;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+}
