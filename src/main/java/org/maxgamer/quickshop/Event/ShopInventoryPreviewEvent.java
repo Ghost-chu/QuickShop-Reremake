d4eed632daa5e58b8a7a8ce71e028743faa3fb3d
Ghost_chu <2908803755@qq.com>
2019-06-09 20:22:06 +0800

    Rename a event
    Former-commit-id: 8d5c33f17aa105b0d74a83736cf0b10cd716485e [formerly 8359416b37c2da5aaf47eae6681a184334d04568]
    Former-commit-id: b5c18cc5e1bdfdf27bf94ba1850eb0f257b3d7ec

--- src/main/java/org/maxgamer/quickshop/Event/ShopInventoryPreviewEvent.java
+++ src/main/java/org/maxgamer/quickshop/Event/ShopInventoryPreviewEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
-public class ShopInventoryPreview extends Event implements Cancellable {
+public class ShopInventoryPreviewEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled;
@@ -19,7 +19,7 @@ public class ShopInventoryPreview extends Event implements Cancellable {
      * @param p         Target plugin
      * @param itemStack The preview item, with preview flag.
      */
-    public ShopInventoryPreview(Player p, ItemStack itemStack) {
+    public ShopInventoryPreviewEvent(Player p, ItemStack itemStack) {
         this.p = p;
         this.itemStack = itemStack;
     }
