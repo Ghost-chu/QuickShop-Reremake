3e92d9c21183d9a534ff57ab818f480ae8fecad5
Ghost_chu <2908803755@qq.com>
2019-06-16 19:57:09 +0800

    Pre-release will sync upload to dev.bukkit.org
    Former-commit-id: f5987a6bf15491b1e86dfefb042328d704743dae [formerly 8e34e09edaa1ba4d6294718ec911ae330e47ad16]
    Former-commit-id: 6a70b6dafe637e4cff939c1ef02af9d6710eed3a

---
+++ snapshot-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Deploy jar to dev.bukkit.org"
+
+# 获取从install完成后输出的JAR文件的名字
+filename="./jarname.txt"
+
+if [[ ! -f $filename ]]
+then
+    echo "The file $filename doesn't exist."
+    exit 1
+fi
+
+# JAR为编译后jar文件名称
+read JAR < $filename
+xToken=$1
+curl -X POST -H "X-Api-Token: $xToken" -F 'metadata={"changelog":"Auto upload by TravisCI, The snapshot may damage your server, not working, destory your saves and databases, backup and use it at your own risk.","gameVersions":[7330,7105],"releaseType":"alpha"}' -F "file=@./push/$JAR" -s "https://dev.bukkit.org/api/projects/320536/upload-file" > fileid.json
+cat fileid.json
+echo "Finished upload to dev.bukkit.org."
+
+#退出脚本，且退出状态码为0
+exit 0
