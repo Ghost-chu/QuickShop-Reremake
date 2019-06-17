fb7876c20a42ff6dbeb8826bf4631ef320e378c6
Ghost_chu <2908803755@qq.com>
2019-06-17 21:21:46 +0800

    Fixed display many bugs
    Former-commit-id: 6f5747e8881f08e0b0e5173257dde52948b9647d [formerly d37aab54dec5b52a45819a24fe0632fbc84b9f7f]
    Former-commit-id: d083c6558cc2e133a134c301c4860603bd21ce72

---
+++ src/main/java/org/maxgamer/quickshop/Shop/ShopProtectionFlag.java
@@ -0,0 +1,11 @@
+package org.maxgamer.quickshop.Shop;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShopProtectionFlag {
+    private String shopLocation;
+    private String itemStackString;
+}
