fad9bdab008e4d30f011dbcc1e15b42d15beb385
Ghost_chu <2908803755@qq.com>
2019-07-14 21:34:33 +0800

    SuperCreate to allow user bypass the protection checks
    Former-commit-id: c51b4f471656d78928e1cc89f6197f5a22aff0e3 [formerly 4f70359f15683fb55e84e5377058dbc6234f6c6e]
    Former-commit-id: 946c774f4a4c3ecfc7eb3b7d500ca8beb6db2d0f

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -20,7 +20,6 @@ import org.maxgamer.quickshop.Database.*;
 import org.maxgamer.quickshop.Database.Database.ConnectionException;
 import org.maxgamer.quickshop.Economy.*;
 import org.maxgamer.quickshop.Listeners.*;
-import org.maxgamer.quickshop.Shop.InventoryPreview;
 import org.maxgamer.quickshop.Shop.Shop;
 import org.maxgamer.quickshop.Shop.ShopLoader;
 import org.maxgamer.quickshop.Shop.ShopManager;
@@ -758,9 +757,7 @@ public class QuickShop extends JavaPlugin {
         getLogger().info("QuickShop is finishing remaining work, this may need a while...");
 
         Util.debugLog("Closing all GUIs...");
-        for (Player player : InventoryPreview.openedGuiPlayers) {
-            if (!player.isOnline())
-                continue;
+        for (Player player : Bukkit.getOnlinePlayers()) {
             player.closeInventory();
         }
 
