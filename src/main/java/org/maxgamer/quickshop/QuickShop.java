09c91fa7ab94ed7da7d1a1de187f3327c769cdcb
Ghost_chu <2908803755@qq.com>
2019-07-15 12:31:55 +0800

    Unregister all listeners when plugin boot failed
    Former-commit-id: 397d7b53a56839bd2559b92593712dfcb87e3631 [formerly 1a7bce710a18d8888ac0280c1007fe9b5cf20d72]
    Former-commit-id: b49fcfe955d929fb2a8ba34351df10c3e5aba03b

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -11,6 +11,7 @@ import lombok.*;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -729,6 +730,8 @@ public class QuickShop extends JavaPlugin {
             getLogger().severe("QuickShop could not hook into a economy/Not found Vault or Reserve!");
             getLogger().severe("QuickShop CANNOT start!");
             bootError = BuiltInSolution.econError();
+            HandlerList.unregisterAll(this);
+            getLogger().severe("Plugin listeners was disabled, please fix the economy issue.");
             return false;
         }
     }
