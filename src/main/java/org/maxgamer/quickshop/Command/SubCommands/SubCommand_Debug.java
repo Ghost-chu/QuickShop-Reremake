c9c2ed10340730b05c34566204042801d2ddf8e9
Ghost_chu <2908803755@qq.com>
2019-08-28 14:43:14 +0800

    Bump 2.4
    Former-commit-id: fce0ca0906d81be9522fc547008c346e6e5642fb [formerly ec4c0fba7db8ca871ccf69b4f50567327e9c53d2]
    Former-commit-id: d6274842d1633e933daca27d81fb9e8124bd88ee

--- src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Debug.java
+++ src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Debug.java
@@ -1,10 +1,14 @@
 package org.maxgamer.quickshop.Command.SubCommands;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.RegisteredListener;
 import org.jetbrains.annotations.*;
 import org.maxgamer.quickshop.Command.CommandProcesser;
 import org.maxgamer.quickshop.QuickShop;
@@ -15,11 +19,40 @@ public class SubCommand_Debug implements CommandProcesser {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
-        return new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("debug");
+        list.add("dev");
+        list.add("devmode");
+        list.add("handlerlist");
+        return list;
     }
 
     @Override
     public void onCommand(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
+        if (cmdArg.length < 1) {
+            switchDebug(sender);
+            return;
+        }
+        switch (cmdArg[0]) {
+            case "debug":
+            case "dev":
+            case "devmode":
+                switchDebug(sender);
+                break;
+            case "handlerlist":
+                if (cmdArg.length < 2) {
+                    sender.sendMessage("You must given a event");
+                    break;
+                }
+                printHandlerList(sender, cmdArg[1]);
+                break;
+            default:
+                sender.sendMessage("Error, no correct args given.");
+                break;
+        }
+    }
+
+    public void switchDebug(@NotNull CommandSender sender) {
         boolean debug = plugin.getConfig().getBoolean("dev-mode");
         if (debug) {
             plugin.getConfig().set("dev-mode", false);
@@ -33,6 +66,21 @@ public class SubCommand_Debug implements CommandProcesser {
             Bukkit.getPluginManager().disablePlugin(plugin);
             Bukkit.getPluginManager().enablePlugin(plugin);
             sender.sendMessage(MsgUtil.getMessage("command.now-debuging"));
+        }
+    }
+
+    public void printHandlerList(@NotNull CommandSender sender, String event) {
+        try {
+            Class clazz = Class.forName(event);
+            Method method = clazz.getMethod("getHandlerList", (Class[]) new Class[0]);
+            Object[] obj = new Object[0];
+            HandlerList list = (HandlerList) method.invoke(null, obj);
+            for (RegisteredListener listener1 : list.getRegisteredListeners()) {
+                sender.sendMessage(ChatColor.AQUA + listener1.getPlugin().getName() + ChatColor.YELLOW + " # " + ChatColor.GREEN + listener1.getListener().getClass().getCanonicalName());
+            }
+        } catch (Throwable th) {
+            sender.sendMessage("ERR " + th.getMessage());
+            th.printStackTrace();
         }
     }
 }
