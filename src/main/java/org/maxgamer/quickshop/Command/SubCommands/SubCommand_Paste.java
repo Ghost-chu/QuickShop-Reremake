e4fb69c01758520a7dfbed5a624d8b9fdf96b3d3
Ghost_chu <2908803755@qq.com>
2019-10-03 11:05:16 +0800

    Local paste

--- src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Paste.java
+++ src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Paste.java
@@ -7,9 +7,14 @@ import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.Command.CommandProcesser;
 import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.Util.Paste;
+import org.maxgamer.quickshop.Util.Util;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class SubCommand_Paste implements CommandProcesser {
     private QuickShop plugin = QuickShop.instance;
@@ -25,13 +30,32 @@ public class SubCommand_Paste implements CommandProcesser {
         new BukkitRunnable() {
             @Override
             public void run() {
+                sender.sendMessage("Please wait...");
+                Paste paste = new Paste(plugin);
+                String pasteText = paste.genNewPaste();
                 try {
-                    sender.sendMessage("Please wait...");
-                    Paste paste = new Paste(plugin);
-                    sender.sendMessage(paste.pasteTheText(paste.genNewPaste()));
-
+                    sender.sendMessage(paste.pasteTheText(pasteText));
                 } catch (Exception err) {
-                    sender.sendMessage("The paste failed, see console for details.");
+                    sender.sendMessage("The paste failed, saving the paste at local location...");
+                    File file = new File(plugin.getDataFolder(),"paste");
+                    file.mkdirs();
+                    file = new File(file, "paste-"+UUID.randomUUID().toString().replaceAll("-","")+".txt");
+                    try {
+                        boolean createResult = file.createNewFile();
+                        Util.debugLog("Create paste file: "+file.getCanonicalPath()+" "+createResult);
+                        FileWriter fwriter = new FileWriter(file);
+                        fwriter.write(pasteText);
+                        fwriter.flush();
+                        fwriter.close();
+                        sender.sendMessage("Paste was saved to your server at: " + file.getAbsolutePath());
+                    } catch (IOException e) {
+                        plugin.getSentryErrorReporter().ignoreThrow();
+                        e.printStackTrace();
+                        sender.sendMessage("Saving failed, output to console...");
+                        plugin.getLogger().info(pasteText);
+                        return;
+                    }
+
                 }
             }
         }.runTaskAsynchronously(plugin);
