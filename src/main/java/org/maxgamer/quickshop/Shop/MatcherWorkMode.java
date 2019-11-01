e2eb6092bf117895312d00e62e39e8f9475baa3f
Ghost_chu <2908803755@qq.com>
2019-11-01 19:00:40 +0800

    Fix #202 #201 and #200
    Former-commit-id: a9adf741f4d435694303246ee50ac04dab94e604 [formerly 387bd91402e2f553d28a59b9e78b5d8dcab7f321]
    Former-commit-id: 52536d2f07dcb6244c29eb7e6247f960df0d3cff

---
+++ src/main/java/org/maxgamer/quickshop/Shop/MatcherWorkMode.java
@@ -0,0 +1,29 @@
+package org.maxgamer.quickshop.Shop;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum MatcherWorkMode {
+    QSMATCHER(0), BUKKITMATCHER(1), JAVAMATCHER(2);
+    public static @NotNull MatcherWorkMode fromID(int id) {
+        for (MatcherWorkMode workMode :  MatcherWorkMode.values()) {
+            if (workMode.id == id) {
+                return workMode;
+            }
+        }
+        return QSMATCHER;
+    }
+
+    public static int toID(@NotNull MatcherWorkMode workMode) {
+        return workMode.id;
+    }
+    MatcherWorkMode(int id) {
+        this.id = id;
+    }
+    private int id;
+    public int toID() {
+        return id;
+    }
+    public int getId() {
+        return id;
+    }
+}
