d3cd7a2dba71f3b8313eb9bc1b4b1609f331b9b1
Ghost_chu <2908803755@qq.com>
2019-11-09 13:38:36 +0800

    Apply InternalListener for ListenerHelper
    Former-commit-id: fb6e5acfefe5711b2c0d14b6396187276c914a0b

--- src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
+++ src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.maxgamer.quickshop.Event.*;
+import org.maxgamer.quickshop.Listeners.ListenerHelper;
 import org.maxgamer.quickshop.QuickShop;
 
 public class InternalListener implements Listener {
@@ -11,28 +12,43 @@ public class InternalListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopCreate(ShopCreateEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
         plugin.log("Player " + event.getPlayer().getName() + " created a shop at location " + event.getShop().getLocation());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopDelete(ShopDeleteEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
         plugin.log("Shop at " + event.getShop().getLocation() + " was removed.");
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopModeratorChanges(ShopModeratorChangedEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
         plugin.log("Shop at location " + event.getShop().getLocation() + " moderator was changed to " + event.getModerator()
                 .toString());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPriceChanges(ShopPriceChangeEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
         plugin.log("Shop at location " + event.getShop().getLocation() + " price was changed from " + event
                 .getOldPrice() + " to " + event.getNewPrice());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPurchase(ShopSuccessPurchaseEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
         plugin.log("Player " + event.getPlayer().getName() + " purchased " + event.getShop().ownerName() + " shop item x" + event
                 .getAmount() + " for " + plugin.getEconomy().format(event.getBalance()) + " (" + plugin.getEconomy()
                 .format(event.getTax()) + " tax).");
--- src/main/java/org/maxgamer/quickshop/Listeners/ListenerHelper.java
+++ src/main/java/org/maxgamer/quickshop/Listeners/ListenerHelper.java
@@ -8,14 +8,24 @@ import java.util.Set;
 public class ListenerHelper {
     private static Set<Class> disabledListener = new HashSet<>();
 
+    /**
+     * Make QuickShop ignore the specify event
+     * @param eventClass The event class, E.g BlockBreakEvent.class
+     */
     public static void disableEvent(@NotNull Class eventClass) {
         disabledListener.add(eventClass);
     }
-
+    /**
+     * Make QuickShop nolonger ignore the specify event
+     * @param eventClass The event class, E.g BlockBreakEvent.class
+     */
     public static void enableEvent(@NotNull Class eventClass) {
         disabledListener.remove(eventClass);
     }
-
+    /**
+     * Check the specify event is disabled
+     * @param eventClass The event class, E.g BlockBreakEvent.class
+     */
     public static boolean isDisabled(Class eventClass) {
         return disabledListener.contains(eventClass);
     }
