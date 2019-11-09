1e6f0eafa16351110026b67b8003e26b23b29475
Ghost_chu <2908803755@qq.com>
2019-11-09 10:48:16 +0800

    More debug and some changes
    Former-commit-id: 665981a4d48113398b1aed968b0576d19e0cb491

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -474,24 +474,24 @@ public class QuickShop extends JavaPlugin {
                         }
 
                         //noinspection unchecked
-                        (getShopManager().getLoadedShops().parallelStream().forEach(shop->{
+                        getShopManager().getLoadedShops().parallelStream().forEach(shop-> {
                             //Check the range has player?
                             int range = getConfig().getInt("shop.display-despawn-range");
                             boolean anyPlayerInRegion = Bukkit.getOnlinePlayers()
                                     .parallelStream()
-                                    .filter(player->player.getWorld().equals(shop.getLocation().getWorld()))
-                                    .anyMatch(player-> player.getLocation().distance(shop.getLocation()) < range);
-                            new BukkitRunnable(){
+                                    .filter(player -> player.getWorld().equals(shop.getLocation().getWorld()))
+                                    .anyMatch(player -> player.getLocation().distance(shop.getLocation()) < range);
+                            new BukkitRunnable() {
                                 @Override
                                 public void run() {
-                                    if(anyPlayerInRegion){
-                                        if(!shop.getDisplay().isSpawned()){
-                                            Util.debugLog("Respawning the shop "+shop.toString()+" the display, cause it was despawned and a player close it");
+                                    if (anyPlayerInRegion) {
+                                        if (!shop.getDisplay().isSpawned()) {
+                                            Util.debugLog("Respawning the shop " + shop.toString() + " the display, cause it was despawned and a player close it");
                                             shop.checkDisplay();
                                         }
-                                    }else{
-                                        if(shop.getDisplay().isSpawned()){
-                                            Util.debugLog("Removing the shop "+shop.toString()+" the display, cause nobody can see it");
+                                    } else {
+                                        if (shop.getDisplay().isSpawned()) {
+                                            Util.debugLog("Removing the shop " + shop.toString() + " the display, cause nobody can see it");
                                             shop.getDisplay().remove();
                                         }
                                     }
