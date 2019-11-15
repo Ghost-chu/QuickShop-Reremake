8aeb6efc449bd421bac535ae5930c0c1354f258b
Ghost_chu <2908803755@qq.com>
2019-11-15 19:03:33 +0800

    Fix #207
    Former-commit-id: 91603644b019c7c2af5f2999fe91a319196421a4

--- src/main/java/org/maxgamer/quickshop/Watcher/LogWatcher.java
+++ src/main/java/org/maxgamer/quickshop/Watcher/LogWatcher.java
@@ -1,28 +1,32 @@
 package org.maxgamer.quickshop.Watcher;
 
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import org.maxgamer.quickshop.QuickShop;
+import org.maxgamer.quickshop.Util.Util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class LogWatcher implements Runnable {
+public class LogWatcher extends BukkitRunnable {
     private Queue<String> logs = new LinkedList<>();
-    private PrintStream ps;
+    private FileWriter logFileWriter = null;
 
     public LogWatcher(QuickShop plugin, File log) {
         try {
             if (!log.exists()) {
                 log.createNewFile();
             }
-            FileOutputStream fos = new FileOutputStream(log, true);
-            this.ps = new PrintStream(fos);
+            logFileWriter = new FileWriter(log);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             plugin.getLogger().severe("Log file was not found!");
@@ -33,20 +37,15 @@ public class LogWatcher implements Runnable {
     }
 
     public void add(@NotNull String s) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                logs.add(s);
-            }
-        }.runTask(QuickShop.instance);
-
+        logs.add(s);
     }
 
+    @SneakyThrows
     public void close() {
-        if (ps != null) {
-            this.ps.close();
+        if (logFileWriter != null) {
+            logFileWriter.flush();
+            logFileWriter.close();
         }
-        ps = null;
     }
 
     public void log(@NonNull String log) {
@@ -57,18 +56,23 @@ public class LogWatcher implements Runnable {
 
     @Override
     public void run() {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (String log : logs) {
-                    if (ps == null) {
-                        continue;
-                    }
-                    ps.print(log + "\n");
-                }
-                logs.clear();
-
+        for (String log : logs) {
+            if (logFileWriter == null) {
+                continue;
             }
-        }.runTaskAsynchronously(QuickShop.instance);
+            try {
+                logFileWriter.write(log + System.getProperty("line.separator"));
+            } catch (IOException ioe) {
+                Util.debugLog("Failed to write log to disk: " + ioe.getMessage() + ", the log was dropped.");
+            }
+        }
+        logs.clear();
+        if (logFileWriter != null) {
+            try {
+                logFileWriter.flush();
+            } catch (IOException ioe) {
+                Util.debugLog("Failed to flush log to disk: " + ioe.getMessage());
+            }
+        }
     }
 }
