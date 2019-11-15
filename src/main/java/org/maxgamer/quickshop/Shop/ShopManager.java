cfedfebd601aafde475a11e67c945201a5e28454
Ghost_chu <2908803755@qq.com>
2019-11-15 20:39:31 +0800

    Fix bug for unlimited shop

--- src/main/java/org/maxgamer/quickshop/Shop/ShopManager.java
+++ src/main/java/org/maxgamer/quickshop/Shop/ShopManager.java
@@ -429,7 +429,12 @@ public class ShopManager {
                 amount = Integer.parseInt(message);
             } catch (NumberFormatException e) {
                 if (message.equalsIgnoreCase(plugin.getConfig().getString("shop.word-for-trade-all-items", "all"))) {
-                    amount = Util.countItems(((ContainerShop) shop).getInventory(), shop.getItem());
+                    if(!shop.isUnlimited()){
+                        amount = Util.countItems(((ContainerShop) shop).getInventory(), shop.getItem());
+                    }else{
+                        amount = Util.countSpace(p.getInventory(), shop.getItem());
+                    }
+
                 } else {
                     p.sendMessage(MsgUtil.getMessage("shop-purchase-cancelled", p));
                     Util.debugLog("Receive the chat " + message + " and it format failed: " + e.getMessage());
