fe61ac6b4841b84d8e1cbcd90f2bcfd77a85062e
Ghost_chu <2908803755@qq.com>
2019-11-16 13:15:16 +0800

    Move display auto-despawn to standalone class
    Former-commit-id: 80f3bdd7d84f0a850de9d0755004d2bb713ddf4d

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -140,6 +140,7 @@ public class QuickShop extends JavaPlugin {
     private ShopProtectionListener shopProtectListener;
     private SyncTaskWatcher syncTaskWatcher;
     private ShopVaildWatcher shopVaildWatcher;
+    private DisplayAutoDespawnWatcher displayAutoDespawnWatcher;
     /**
      * Use SpoutPlugin to get item / block names
      */
@@ -482,42 +483,8 @@ public class QuickShop extends JavaPlugin {
 
         if (this.display) {
             if (getConfig().getBoolean("shop.display-auto-despawn")) {
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        if (getShopManager().getLoadedShops() == null) {
-                            return;
-                        }
-
-                        getShopManager().getLoadedShops().parallelStream().forEach(shop -> {
-                            //Check the range has player?
-                            int range = getConfig().getInt("shop.display-despawn-range");
-                            boolean anyPlayerInRegion = Bukkit.getOnlinePlayers()
-                                    .parallelStream()
-                                    .filter(player -> player.getWorld().equals(shop.getLocation().getWorld()))
-                                    .anyMatch(player -> player.getLocation().distance(shop.getLocation()) < range);
-                            new BukkitRunnable() {
-                                @Override
-                                public void run() {
-                                    if (shop.getDisplay() == null) {
-                                        return;
-                                    }
-                                    if (anyPlayerInRegion) {
-                                        if (!shop.getDisplay().isSpawned()) {
-                                            Util.debugLog("Respawning the shop " + shop.toString() + " the display, cause it was despawned and a player close it");
-                                            shop.checkDisplay();
-                                        }
-                                    } else {
-                                        if (shop.getDisplay().isSpawned()) {
-                                            Util.debugLog("Removing the shop " + shop.toString() + " the display, cause nobody can see it");
-                                            shop.getDisplay().remove();
-                                        }
-                                    }
-                                }
-                            }.runTask(QuickShop.instance);
-                        });
-                    }
-                }.runTaskTimerAsynchronously(this, 0, getConfig().getInt("shop.display-check-time"));
+                this.displayAutoDespawnWatcher = new DisplayAutoDespawnWatcher(this);
+                this.displayAutoDespawnWatcher.runTaskTimerAsynchronously(this, 0, getConfig().getInt("shop.display-check-time"));
             }
         }
     }
