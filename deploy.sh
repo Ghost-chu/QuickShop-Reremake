cd4640c948371354ff3bf26606f3f8cc285bae51
Ghost_chu <2908803755@qq.com>
2019-11-23 13:36:37 +0800

    Another fixes?
    Former-commit-id: 1c4283865bafdfc039909048e1c2d3bbe81df95d

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "Deploy jar to dev.bukkit.org"
 
-# 閼惧嘲褰囨禒宸宯stall鐎瑰本鍨氶崥搴ょ翻閸戣櫣娈慗AR閺傚洣娆㈤惃鍕倳鐎涳拷
 filename="./jarname.txt"
 
 if [ ! -f $filename ]; then
@@ -9,12 +8,10 @@ if [ ! -f $filename ]; then
   exit 1
 fi
 
-# JAR娑撹櫣绱拠鎴濇倵jar閺傚洣娆㈤崥宥囆�
 read JAR <$filename
 xToken=$1
 curl -X POST -H "X-Api-Token: $xToken" -F 'metadata={"changelog":"Auto upload by TravisCI, see update details on spigotmc.org","gameVersions":[7330,7105],"releaseType":"release"}' -F "file=@./devbukkit/$JAR" -s "https://dev.bukkit.org/api/projects/320536/upload-file" >fileid.json
 cat fileid.json
 echo "Finished upload to dev.bukkit.org."
 
-#闁拷閸戦缚鍓奸張顒婄礉娑撴棃锟斤拷閸戣櫣濮搁幀浣虹垳娑擄拷0
 exit 0
