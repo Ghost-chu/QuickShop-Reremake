15511aa541965007773da4153cc5e12828dedba7
portlek <hsndmrts_@outlook.com>
2019-11-24 02:49:42 +0300

    add more null-safety, code optimizing and simplifying
    Former-commit-id: 1f141a45c8ba1653a3f5bd6817d2984b85ed198e

--- src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Reload.java
+++ src/main/java/org/maxgamer/quickshop/Command/SubCommands/SubCommand_Reload.java
@@ -1,9 +1,7 @@
 package org.maxgamer.quickshop.Command.SubCommands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.Command.CommandProcesser;
 import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.Util.MsgUtil;
@@ -12,17 +10,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubCommand_Reload implements CommandProcesser {
-    private QuickShop plugin = QuickShop.instance;
 
+    private final QuickShop plugin = QuickShop.instance;
+
+    @NotNull
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         return new ArrayList<>();
     }
 
     @Override
     public void onCommand(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         sender.sendMessage(MsgUtil.getMessage("command.reloading", sender));
-        Bukkit.getPluginManager().disablePlugin(plugin);
-        Bukkit.getPluginManager().enablePlugin(plugin);
+        plugin.getServer().getPluginManager().disablePlugin(plugin);
+        plugin.getServer().getPluginManager().enablePlugin(plugin);
     }
 }
