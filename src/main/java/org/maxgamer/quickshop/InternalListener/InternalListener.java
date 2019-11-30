4abea807f22dcca17f13d7a795752609caf77d34
Ghost_chu <2908803755@qq.com>
2019-11-30 20:24:16 +0800

    Apply webhook for some events
    Former-commit-id: 70e0e45822842d19c4ec2a00606cd9297a340a38

--- src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
+++ src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
@@ -18,6 +18,7 @@ public class InternalListener implements Listener {
             return;
         }
         plugin.log("Player " + event.getPlayer().getName() + " created a shop at location " + event.getShop().getLocation());
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -26,6 +27,7 @@ public class InternalListener implements Listener {
             return;
         }
         plugin.log("Shop at " + event.getShop().getLocation() + " was removed.");
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -35,6 +37,7 @@ public class InternalListener implements Listener {
         }
         plugin.log("Shop at location " + event.getShop().getLocation() + " moderator was changed to " + event.getModerator()
                 .toString());
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -44,6 +47,7 @@ public class InternalListener implements Listener {
         }
         plugin.log("Shop at location " + event.getShop().getLocation() + " price was changed from " + event
                 .getOldPrice() + " to " + event.getNewPrice());
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -51,9 +55,41 @@ public class InternalListener implements Listener {
         if (ListenerHelper.isDisabled(event.getClass())) {
             return;
         }
-        plugin.log("Player " + event.getPlayer().getName() + " purchased " + event.getShop().ownerName() + " shop "+event.getShop()+" for items x" + event
+        plugin.log("Player " + event.getPlayer().getName() + " purchased " + event.getShop().ownerName() + " shop " + event.getShop() + " for items x" + event
                 .getAmount() + " for " + plugin.getEconomy().format(event.getBalance()) + " (" + plugin.getEconomy()
                 .format(event.getTax()) + " tax).");
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
     }
 
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void shopLoad(ShopLoadEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void shopUnload(ShopUnloadEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void shopSignUpdate(ShopSignUpdatedEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void shopUpdate(ShopUpdateEvent event) {
+        if (ListenerHelper.isDisabled(event.getClass())) {
+            return;
+        }
+        plugin.getWebhookHelper().noticeOnlyAsync(event);
+    }
 }
