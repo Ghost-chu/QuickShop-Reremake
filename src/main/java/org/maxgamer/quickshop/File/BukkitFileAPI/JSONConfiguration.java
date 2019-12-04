25ada0731d9bea8dd3e758885f85245d7c86b6d7
portlek <hsndmrts_@outlook.com>
2019-12-04 16:05:41 +0300

    Update JSONConfiguration.java

--- src/main/java/org/maxgamer/quickshop/File/BukkitFileAPI/JSONConfiguration.java
+++ src/main/java/org/maxgamer/quickshop/File/BukkitFileAPI/JSONConfiguration.java
@@ -94,7 +94,7 @@ public final class JSONConfiguration extends FileConfiguration {
                     finalPath = path + ".";
                 }
 
-                convertMapsToJsonObject(((ConfigurationSection) value).getValues(false), finalPath + "." + key);
+                convertMapsToJsonObject(((ConfigurationSection) value).getValues(false), finalPath + key);
             } else {
 
             }
