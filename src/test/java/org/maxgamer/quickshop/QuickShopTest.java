435f24b96071a457d45fb38855d4d9fea908da88
portlek <hsndmrts_@outlook.com>
2019-12-12 16:04:22 +0300

    test
    Former-commit-id: edc413983531f70cae15f0b2a561c837e8719f99

--- src/test/java/org/maxgamer/quickshop/QuickShopTest.java
+++ src/test/java/org/maxgamer/quickshop/QuickShopTest.java
@@ -3,22 +3,18 @@ package org.maxgamer.quickshop;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
-public final class QuickShopTest extends JavaPlugin {
+public class QuickShopTest extends JavaPlugin {
 
     public QuickShopTest() {
         super();
     }
 
-    protected QuickShopTest(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-        super(
-            loader,
-            description,
-            new File("build"),
-            file
-        );
+    public QuickShopTest(@NotNull JavaPluginLoader loader, @NotNull PluginDescriptionFile description,
+                         @NotNull File dataFolder, @NotNull File file) {
+        super(loader, description, new File("build"), file);
     }
-
 }
