2e781bd8f446fd945432b5088998a20f183720cc
portlek <hsndmrts_@outlook.com>
2019-12-12 19:24:26 +0300

    size reduce
    Former-commit-id: ad8c320956d404dae78cac6c82395f17148ae142

--- src/test/java/org/maxgamer/quickshop/QuickShopTest.java
+++ src/test/java/org/maxgamer/quickshop/QuickShopTest.java
@@ -3,7 +3,6 @@ package org.maxgamer.quickshop;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
