8f231aeb7ef9c6514d494d51121430b0ac6e3c0f
Ghost_chu <2908803755@qq.com>
2019-12-12 14:37:25 +0800

    EngineHub paster support, add some useful utils grab from other project
    Former-commit-id: 5617cf827e1e0915ef301105023d4d7dc3a5327f

---
+++ src/main/java/org/maxgamer/quickshop/Util/Paste/PasteInterface.java
@@ -0,0 +1,7 @@
+package org.maxgamer.quickshop.Util.Paste;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface PasteInterface {
+    String pasteTheText(@NotNull String text) throws Exception;
+}
