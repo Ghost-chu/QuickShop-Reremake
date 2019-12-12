ec61b653791f21b2742d6c1f7bf9d29ad54e9ace
portlek <hsndmrts_@outlook.com>
2019-12-12 15:34:33 +0300

    tests

--- src/test/java/org/maxgamer/quickshop/File/JSONFileTest.java
+++ src/test/java/org/maxgamer/quickshop/File/JSONFileTest.java
@@ -1,9 +1,13 @@
 package org.maxgamer.quickshop.File;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 import org.maxgamer.quickshop.QuickShopTest;
 
 class JSONFileTest {
@@ -16,6 +20,7 @@ class JSONFileTest {
         MockBukkit.mock();
         plugin = MockBukkit.load(QuickShopTest.class);
         json = new JSONFile(plugin, "messages");
+        json.create();
     }
 
     @Test
@@ -24,9 +29,23 @@ class JSONFileTest {
             return;
         }
 
-        json.create();
-        json.set("test.test", "test");
-        json.save();
+        new Assertion<>(
+            "There is no 'shop-staff-cleared' path",
+            json.get("shop-staff-cleared"),
+            new IsEqual<>("&aSuccessfully removed all staff for your shop.")
+        ).affirm();
+
+        new Assertion<>(
+            "The value of the path is not a String",
+            json.get("shop-staff-cleared"),
+            new IsInstanceOf(String.class)
+        ).affirm();
+
+        new Assertion<>(
+            "There is no 'signs.item' path",
+            json.get("signs.item"),
+            new IsEqual<>("{0}")
+        ).affirm();
     }
 
     @AfterEach
