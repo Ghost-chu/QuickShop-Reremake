76b06adb50e555ec88f9a7889650a828b29841a6
Ghost_chu <2908803755@qq.com>
2019-12-12 21:02:51 +0800

    Add 7667[1.15] on bukkitdev site version number to auto-upload script
    Former-commit-id: 2a7d5e1b4f9bb4d1def8e44a53648ed5b515fb3c

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ fi
 
 read -r JAR <$filename
 xToken=$1
-curl -X POST -H "X-Api-Token: $xToken" -F 'metadata={"changelog":"Auto upload by TravisCI, see update details on spigotmc.org","gameVersions":[7330,7105],"releaseType":"release"}' -F "file=@./devbukkit/$JAR" -s "https://dev.bukkit.org/api/projects/320536/upload-file" >fileid.json
+curl -X POST -H "X-Api-Token: $xToken" -F 'metadata={"changelog":"Auto upload by TravisCI, see update details on spigotmc.org","gameVersions":[7667,7330,7105],"releaseType":"release"}' -F "file=@./devbukkit/$JAR" -s "https://dev.bukkit.org/api/projects/320536/upload-file" >fileid.json
 cat fileid.json
 echo "Finished upload to dev.bukkit.org."
 
