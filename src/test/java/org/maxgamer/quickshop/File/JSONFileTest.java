4fb7da596920b1bf5a61405e0850fc8e622d525d
portlek <hsndmrts_@outlook.com>
2019-12-12 16:04:22 +0300

    test
    Former-commit-id: edc413983531f70cae15f0b2a561c837e8719f99

--- src/test/java/org/maxgamer/quickshop/File/JSONFileTest.java
+++ src/test/java/org/maxgamer/quickshop/File/JSONFileTest.java
@@ -1,9 +1,14 @@
 package org.maxgamer.quickshop.File;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 import org.maxgamer.quickshop.QuickShopTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class JSONFileTest {
 
@@ -16,6 +21,15 @@ class JSONFileTest {
         plugin = MockBukkit.load(QuickShopTest.class);
         json = new JSONFile(plugin, "messages");
         json.create();
+    }
+
+    @Test
+    void get() {
+        new Assertion<>(
+            "There is no 'signs.item' path",
+            json.get("signs.item"),
+            new IsEqual<>("{0}")
+        ).affirm();
     }
 
     @AfterEach
