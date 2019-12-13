a07cc3a92e48acb4914745807fd12103ccf718ad
portlek <hsndmrts_@outlook.com>
2019-12-13 09:39:26 +0300

    Package changes
    Former-commit-id: 2eface59355a75131aa187eb91cf0d18886c8a22

--- src/main/java/org/maxgamer/quickshop/NonQuickShopStuffs/com/dumbtruckman/JsonConfiguration/JSONConfigurationOptions.java
+++ src/main/java/org/maxgamer/quickshop/NonQuickShopStuffs/com/dumbtruckman/JsonConfiguration/JSONConfigurationOptions.java
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-package org.maxgamer.quickshop.File.BukkitFileAPI;
+package org.maxgamer.quickshop.NonQuickShopStuffs.com.dumbtruckman.JsonConfiguration;
 
 import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.jetbrains.annotations.NotNull;
