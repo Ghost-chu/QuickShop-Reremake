e00a9c301abeedbfbfbdd90de43cfe3348e4a7c5
portlek <hsndmrts_@outlook.com>
2019-12-13 12:32:11 +0300

    Improvement for IFile class
    Former-commit-id: c3b2b924c5e72624cbe35cdf422eeb29136cd533

--- src/main/java/org/maxgamer/quickshop/File/FileEnvelope.java
+++ src/main/java/org/maxgamer/quickshop/File/FileEnvelope.java
@@ -1,15 +1,24 @@
 package org.maxgamer.quickshop.File;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.Mock.MckFileConfiguration;
 import org.maxgamer.quickshop.Util.Copied;
+import org.maxgamer.quickshop.Util.Location.LocationOf;
+import org.maxgamer.quickshop.Util.Location.StringOf;
+import org.maxgamer.quickshop.Util.Util;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public abstract class FileEnvelope implements IFile {
 
@@ -79,7 +88,7 @@ public abstract class FileEnvelope implements IFile {
 
             fileConfiguration.save(file);
         } catch (Exception exception) {
-            throw new RuntimeException(exception);
+            throw new IllegalStateException(exception);
         }
     }
 
@@ -89,9 +98,140 @@ public abstract class FileEnvelope implements IFile {
         return fileConfiguration.get(path);
     }
 
+    @NotNull
+    @Override
+    public Object get(@NotNull String path, @NotNull Object fallback) {
+        final Object object = fileConfiguration.get(path, fallback);
+
+        if (object == null) {
+            return fallback;
+        }
+
+        return object;
+    }
+
+    @NotNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getOrSet(@NotNull String path, @NotNull T fallback) {
+        final T object = (T) fileConfiguration.get(path, null);
+
+        if (object != null) {
+            return object;
+        }
+
+        set(path, fallback);
+
+        return fallback;
+    }
+
+    @NotNull
+    @Override
+    public Location getLocation(@NotNull String path) {
+        return new LocationOf(
+            getString(path).orElse("")
+        ).value();
+    }
+
+    @Override
+    public void setLocation(@NotNull String path, @NotNull Location location) {
+        set(
+            path,
+            new StringOf(location).asString()
+        );
+    }
+
+    @NotNull
+    @Override
+    public ItemStack getCustomItemStack(@NotNull String path) {
+        try {
+            return Util.deserialize(path);
+        } catch (Exception exception) {
+            return new ItemStack(Material.AIR);
+        }
+    }
+
+    @Override
+    public void setCustomItemStack(@NotNull String path, @NotNull ItemStack itemStack) {
+        fileConfiguration.set(path, Util.serialize(itemStack));
+    }
+
+    @NotNull
+    @Override
+    public Optional<String> getString(@NotNull String path) {
+        return Optional.ofNullable(fileConfiguration.getString(path));
+    }
+
     @Override
     public void set(@NotNull String path, @NotNull Object object) {
         fileConfiguration.set(path, object);
+        save();
+    }
+
+    @NotNull
+    @Override
+    public List<String> getStringList(@NotNull String path) {
+        return fileConfiguration.getStringList(path);
+    }
+
+    @Override
+    public int getInt(@NotNull String path) {
+        return fileConfiguration.getInt(path);
+    }
+
+    @Override
+    public double getDouble(@NotNull String path) {
+        return fileConfiguration.getDouble(path);
+    }
+
+    @Override
+    public long getLong(@NotNull String path) {
+        return fileConfiguration.getLong(path);
+    }
+
+    @Override
+    public byte getByte(@NotNull String path) {
+        return (byte) fileConfiguration.getInt(path);
+    }
+
+    @Override
+    public short getShort(@NotNull String path) {
+        return (short) fileConfiguration.getInt(path);
+    }
+
+    @Override
+    public boolean getBoolean(@NotNull String path) {
+        return fileConfiguration.getBoolean(path);
+    }
+
+    @NotNull
+    @Override
+    public ConfigurationSection createSection(@NotNull String path) {
+        final ConfigurationSection configurationSection = fileConfiguration.createSection(path);
+
+        save();
+
+        return configurationSection;
+    }
+
+    @NotNull
+    @Override
+    public ConfigurationSection getSection(@NotNull String path) {
+        final ConfigurationSection configurationSection = fileConfiguration.getConfigurationSection(path);
+
+        return configurationSection == null ? new MckFileConfiguration() : configurationSection;
+    }
+
+    @NotNull
+    @Override
+    public ConfigurationSection getOrCreateSection(@NotNull String path) {
+        ConfigurationSection section = getSection(path);
+
+        if (section instanceof MckFileConfiguration) {
+            section = createSection(path);
+        }
+
+        return section;
     }
 
 }
---
+++ src/test/java/org/maxgamer/quickshop/File/FileEnvelopeTest.java
@@ -0,0 +1,257 @@
+package org.maxgamer.quickshop.File;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.maxgamer.quickshop.Mock.MckFileConfiguration;
+import org.maxgamer.quickshop.QuickShopTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileEnvelopeTest {
+
+    private final List<String> test = new ArrayList<>();
+    private QuickShopTest plugin;
+    private IFile json;
+    private IFile yaml;
+
+    @BeforeEach
+    void start() {
+        MockBukkit.mock();
+        plugin = MockBukkit.load(QuickShopTest.class);
+        json = new JSONFile(plugin, "messages");
+        json.create();
+        yaml = new YAMLFile(plugin, "messages");
+        yaml.create();
+        test.add("test");
+        json.set("test.list", test);
+        yaml.set("test.list", test);
+        json.set("test.int", 13);
+        yaml.set("test.int", 13);
+        json.set("test.boolean", true);
+        yaml.set("test.boolean", true);
+        json.set("test.short", (short) 1);
+        yaml.set("test.short", (short) 1);
+        json.set("test.long", (long) 1);
+        yaml.set("test.long", (long) 1);
+        json.set("test.byte", (byte) 1);
+        yaml.set("test.byte", (byte) 1);
+        json.set("test.double", (double) 1);
+        yaml.set("test.double", (double) 1);
+    }
+
+    @Test
+    void get() {
+        assertEquals(
+            "{0}",
+            json.get("signs.item")
+        );
+        assertEquals(
+            "{0}",
+            yaml.get("signs.item")
+        );
+    }
+
+    @Test
+    void getString() {
+        final Optional<String> optionalS = json.getString("shop-staff-cleared");
+        final Optional<String> optionalSS = yaml.getString("shop-staff-cleared");
+
+        if (!optionalS.isPresent() || !optionalSS.isPresent()) {
+            throw new IllegalArgumentException("There no path called 'shop-staff-cleared'");
+        }
+
+        assertEquals(
+            "&aSuccessfully removed all staff for your shop.",
+            optionalS.get()
+        );
+        assertEquals(
+            "&aSuccessfully removed all staff for your shop.",
+            optionalSS.get()
+        );
+    }
+
+    @Test
+    void set() {
+        json.set("test.test", "test");
+        yaml.set("test.test", "test");
+        assertEquals(
+            "{0}",
+            json.get("signs.item")
+        );
+        assertEquals(
+            "{0}",
+            yaml.get("signs.item")
+        );
+    }
+
+    @Test
+    void create() {
+        json.create();
+        yaml.create();
+    }
+
+    @Test
+    void save() {
+        json.save();
+        yaml.save();
+    }
+
+    @Test
+    void getWithFallback() {
+        assertEquals(
+            "&aSuccessfully removed all staff for your shop.",
+            json.get("shop-staff-cleared", "test")
+        );
+        assertEquals(
+            "&aSuccessfully removed all staff for your shop.",
+            yaml.get("shop-staff-cleared", "test")
+        );
+    }
+
+    @Test
+    void getOrSet() {
+        assertEquals(
+            "test",
+            json.getOrSet("test.test123", "test")
+        );
+        assertEquals(
+            "test",
+            yaml.getOrSet("test.test123", "test")
+        );
+        assertEquals(
+            "test",
+            json.getOrSet("test.test123", "test")
+        );
+        assertEquals(
+            "test",
+            yaml.getOrSet("test.test123", "test")
+        );
+    }
+
+    @Test
+    void getStringList() {
+        assertEquals(
+            test,
+            json.getStringList("test.list")
+        );
+        assertEquals(
+            test,
+            yaml.getStringList("test.list")
+        );
+    }
+
+    @Test
+    void getInt() {
+        assertEquals(
+            13,
+            json.getInt("test.int")
+        );
+        assertEquals(
+            13,
+            yaml.getInt("test.int")
+        );
+    }
+
+    @Test
+    void getDouble() {
+        assertEquals(
+            1,
+            json.getDouble("test.short")
+        );
+        assertEquals(
+            1,
+            yaml.getDouble("test.short")
+        );
+    }
+
+    @Test
+    void getLong() {
+        assertEquals(
+            1,
+            json.getLong("test.short")
+        );
+        assertEquals(
+            1,
+            yaml.getLong("test.short")
+        );
+    }
+
+    @Test
+    void getByte() {
+        assertEquals(
+            (byte) 1,
+            json.getByte("test.short")
+        );
+        assertEquals(
+            (byte) 1,
+            yaml.getByte("test.short")
+        );
+    }
+
+    @Test
+    void getShort() {
+        assertEquals(
+            (short) 1,
+            json.getShort("test.short")
+        );
+        assertEquals(
+            (short) 1,
+            yaml.getShort("test.short")
+        );
+    }
+
+    @Test
+    void getBoolean() {
+        assertTrue(
+            json.getBoolean("test.boolean")
+        );
+        assertTrue(
+            yaml.getBoolean("test.boolean")
+        );
+    }
+
+    @Test
+    void createSection() {
+        json.createSection("test.section");
+        yaml.createSection("test.section");
+    }
+
+    @Test
+    void getSection() {
+        assertTrue(
+            () -> !(json.getSection("test.section") instanceof MckFileConfiguration)
+        );
+        assertTrue(
+            () -> !(yaml.getSection("test.section") instanceof MckFileConfiguration)
+        );
+    }
+
+    @Test
+    void getOrCreateSection() {
+        assertTrue(
+            () -> !(json.getOrCreateSection("test.section") instanceof MckFileConfiguration)
+        );
+        assertTrue(
+            () -> !(yaml.getOrCreateSection("test.section") instanceof MckFileConfiguration)
+        );
+    }
+
+    @AfterEach
+    void stop() {
+        json.save();
+        yaml.save();
+        MockBukkit.unload();
+    }
+
+}
