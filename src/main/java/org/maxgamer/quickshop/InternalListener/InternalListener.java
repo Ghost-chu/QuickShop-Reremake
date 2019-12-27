ed7ffa872d8f2ca7dc25574a82e7e76d20402092
Ectabro <cloud.jul@gmail.com>
2019-12-27 17:15:09 +0100

    Update InternalListener.java

--- src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
+++ src/main/java/org/maxgamer/quickshop/InternalListener/InternalListener.java
@@ -71,6 +71,9 @@ public class InternalListener implements Listener {
         if (ListenerHelper.isDisabled(event.getClass())) {
             return;
         }
+        if ((event.shop).getShopType() == ShopType.BUYING ) {
+        	plugin.log("sold");
+        }
         plugin.log("Player " + event.getPlayer().getName() + " purchased " + event.getShop().ownerName() + " shop "+event.getShop()+" for items x" + event
             .getAmount() + " for " + plugin.getEconomy().format(event.getBalance()) + " (" + plugin.getEconomy()
             .format(event.getTax()) + " tax).");
