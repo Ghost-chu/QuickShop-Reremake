42eaa36a3dda0cb6dbb62241e8cdb8f3f1392bea
Ghost_chu <2908803755@qq.com>
2020-01-06 13:33:49 +0800

    Dump
    Former-commit-id: d0d3acbfd2034ee222be392664940047f5f8b07f

--- src/main/java/org/maxgamer/quickshop/Database/DatabaseHelper.java
+++ src/main/java/org/maxgamer/quickshop/Database/DatabaseHelper.java
@@ -123,10 +123,10 @@ public class DatabaseHelper {
     private boolean createMessagesTable() throws SQLException {
         Statement st = db.getConnection().createStatement();
         String createTable = "CREATE TABLE " + QuickShop.instance.getDbPrefix()
-                + "messages (owner  VARCHAR(255) NOT NULL, message TEXT(25) NOT NULL, time  BIGINT(32) NOT NULL );";
+                + "messages (owner  VARCHAR(255) NOT NULL, message  TEXT(25) NOT NULL, time  BIGINT(32) NOT NULL );";
         if (plugin.getDatabase().getCore() instanceof MySQLCore) {
             createTable = "CREATE TABLE " + QuickShop.instance.getDbPrefix()
-                    + "messages (owner  VARCHAR(255) NOT NULL, message TEXT(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL , time  BIGINT(32) NOT NULL );";
+                    + "messages (owner  VARCHAR(255) NOT NULL, message  TEXT(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL , time  BIGINT(32) NOT NULL );";
         }
         return st.execute(createTable);
     }
