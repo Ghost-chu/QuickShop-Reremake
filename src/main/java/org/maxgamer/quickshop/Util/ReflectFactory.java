7cece0c6c5177614b77c1ff5a6ffc8920ef2c935
Ghost_chu <2908803755@qq.com>
2020-01-10 17:11:16 +0800

    Add api to get MCVersion and Improve MojangAPI
    Former-commit-id: fba7ebb23363f75b7445b13773a25d57e1e919f4

---
+++ src/main/java/org/maxgamer/quickshop/Util/ReflectFactory.java
@@ -0,0 +1,38 @@
+/*
+ * This file is a part of project QuickShop, the name is ReflectFactory.java
+ * Copyright (C) Ghost_chu <https://github.com/Ghost-chu>
+ * Copyright (C) Bukkit Commons Studio and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.maxgamer.quickshop.Util;
+
+import org.bukkit.Bukkit;
+
+import java.lang.reflect.Field;
+
+public class ReflectFactory {
+    public static String getServerVersion(){
+        try {
+            Field consoleField = Bukkit.getServer().getClass().getDeclaredField("console");
+            consoleField.setAccessible(true); // protected
+            Object console = consoleField.get(Bukkit.getServer()); // dedicated server
+            return String.valueOf(console.getClass().getSuperclass().getMethod("getVersion").invoke(console));
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Unknown";
+        }
+    }
+}
