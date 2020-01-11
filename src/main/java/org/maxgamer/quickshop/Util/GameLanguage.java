d972197bbf71073d528017f6eca59d2a719a89db
Ghost_chu <2908803755@qq.com>
2020-01-11 14:01:17 +0800

    Ooops
    Former-commit-id: 95a6f0cc53068231e43b103754f6d15a5b26a0b3

--- src/main/java/org/maxgamer/quickshop/Util/GameLanguage.java
+++ src/main/java/org/maxgamer/quickshop/Util/GameLanguage.java
@@ -26,8 +26,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.maxgamer.quickshop.Util.MojangAPI.AssetJson;
 import org.maxgamer.quickshop.Util.MojangAPI.MojangAPI;
-import org.maxgamer.quickshop.Util.MojangAPI.VersionJson;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -36,14 +36,19 @@ import java.util.Locale;
 
 public class GameLanguage {
     private Locale locale = Locale.getDefault();
-    private  String languageCode;
+    private String languageCode;
     private MojangAPI mojangAPI = new MojangAPI();
     private @Nullable JsonObject lang = null;
     private YamlConfiguration yamlConfiguration = new YamlConfiguration();
-    public GameLanguage(@NotNull String languageCode){
+
+    public GameLanguage(@NotNull String languageCode) {
 
         try {
-            yamlConfiguration.load(new File(Util.getCacheFolder(),"lang.cache"));
+            File cacheFile = new File(Util.getCacheFolder(), "lang.cache");
+            if (!cacheFile.exists()) {
+                cacheFile.createNewFile();
+            }
+            yamlConfiguration.load(new File(Util.getCacheFolder(), "lang.cache"));
             boolean needUpdateCache = false;
             String cachingServerVersion = yamlConfiguration.getString("ver");
             String cachingLanguageHash = yamlConfiguration.getString("hash");
@@ -52,130 +57,169 @@ public class GameLanguage {
             if ("default".equals(languageCode)) {
                 languageCode = locale.getLanguage() + "_" + locale.getCountry();
             }
-            if(!languageCode.equals(cachingLanguageName)){
+            if (!languageCode.equals(cachingLanguageName)) {
                 cachingLanguageName = languageCode;
                 needUpdateCache = true;
             }
             this.languageCode = languageCode.toLowerCase();
             String serverVersion = ReflectFactory.getServerVersion();
-            if(!serverVersion.equals(cachingServerVersion)){
+            if (!serverVersion.equals(cachingServerVersion)) {
                 cachingServerVersion = serverVersion;
                 needUpdateCache = true;
             }
+            if(cachingLanguageHash == null || cachingLanguageHash.isEmpty()){
+                needUpdateCache = true;
+            }
 
-            if(needUpdateCache){
-                String versionJ = mojangAPI.getVersionJson(cachingServerVersion);
-                if (versionJ != null) {
-                    VersionJson versionJson = new VersionJson(versionJ);
+            if (needUpdateCache) {
+                String assetJson = mojangAPI.getAssetIndexJson(cachingServerVersion);
+                if (assetJson != null) {
+                    AssetJson versionJson = new AssetJson(assetJson);
                     String hash = versionJson.getLanguageHash(this.languageCode);
                     if (hash != null) {
                         cachingLanguageHash = hash;
                         String langJson = mojangAPI.downloadTextFileFromMojang(hash);
-                        if(langJson != null){
-                            new Copied(new File(Util.getCacheFolder(),hash)).accept(new ByteArrayInputStream(langJson.getBytes(StandardCharsets.UTF_8)));
+                        if (langJson != null) {
+                            new Copied(new File(Util.getCacheFolder(), hash)).accept(new ByteArrayInputStream(langJson.getBytes(StandardCharsets.UTF_8)));
+                        }else{
+                            Util.debugLog("Cannot download file.");
                         }
+                    }else{
+                        Util.debugLog("Cannot get file hash for language "+this.languageCode);
                     }
+                }else{
+                    Util.debugLog("Cannot get version json.");
                 }
             }
-            yamlConfiguration.set("ver",cachingServerVersion);
-            yamlConfiguration.set("hash",cachingLanguageHash);
-            yamlConfiguration.set("lang",cachingLanguageName);
-            yamlConfiguration.save(new File(Util.getCacheFolder(),"lang.cache"));
+            yamlConfiguration.set("ver", cachingServerVersion);
+            yamlConfiguration.set("hash", cachingLanguageHash);
+            yamlConfiguration.set("lang", cachingLanguageName);
+            yamlConfiguration.save(cacheFile);
             String json = null;
-            if(cachingLanguageHash!=null){
-               json = Util.readToString(new File(Util.getCacheFolder(),cachingLanguageHash));
+            if (cachingLanguageHash != null) {
+                json = Util.readToString(new File(Util.getCacheFolder(), cachingLanguageHash));
             }
-            if(json != null && !json.isEmpty()){
+            if (json != null && !json.isEmpty()) {
                 lang = new JsonParser().parse(json).getAsJsonObject();
             }
 
-        }catch (Exception e){
-            Util.debugLog(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
     @NotNull
-    public String getItem(@NotNull Material material){
+    public String getItem(@NotNull Material material) {
         String name = Util.prettifyText(material.name());
-        if(lang == null){
+        if (lang == null) {
             return name;
         }
-        JsonObject obj = lang.get("item").getAsJsonObject().get("minecraft").getAsJsonObject();
-        String jsonName = obj.get(material.name().toLowerCase()).getAsString();
-        if(jsonName == null || jsonName.isEmpty()){
-            return name;
-        }
-        return jsonName;
-    }
-    @NotNull
-    public String getBlock(@NotNull Material material){
-        String name = Util.prettifyText(material.name());
-        if(lang == null){
-            return name;
-        }
-        JsonObject obj = lang.get("block").getAsJsonObject().get("minecraft").getAsJsonObject();
-        String jsonName = obj.get(material.name().toLowerCase()).getAsString();
-        if(jsonName == null || jsonName.isEmpty()){
-            return name;
-        }
-        return jsonName;
-    }
-    @NotNull
-    public String getEntity(@NotNull Material material){
-        String name = Util.prettifyText(material.name());
-        if(lang == null){
-            return name;
-        }
-        JsonObject obj = lang.get("entity").getAsJsonObject().get("minecraft").getAsJsonObject();
-        String jsonName = obj.get(material.name().toLowerCase()).getAsString();
-        if(jsonName == null || jsonName.isEmpty()){
-            return name;
-        }
-        return jsonName;
-    }
-    @NotNull
-    public String getPotion(@NotNull PotionEffectType effect){
-        String name = Util.prettifyText(effect.getName());
-        if(lang == null){
-            return name;
-        }
-        JsonObject obj = lang.get("effect").getAsJsonObject().get("minecraft").getAsJsonObject();
-        String jsonName = obj.get(effect.getName().toLowerCase()).getAsString();
-        if(jsonName == null || jsonName.isEmpty()){
-            return name;
-        }
-        return jsonName;
-    }
-    @NotNull
-    public String getEnchantment(@NotNull String enchantmentName){
-        String name = Util.prettifyText(enchantmentName);
-        if(lang == null){
-            return name;
-        }
-        JsonObject obj = lang.get("enchantment").getAsJsonObject().get("minecraft").getAsJsonObject();
-        String jsonName = obj.get(enchantmentName.toLowerCase()).getAsString();
-        if(jsonName == null || jsonName.isEmpty()){
-            return name;
-        }
-        return jsonName;
-    }
-    @Nullable
-    public String getCustom(@NotNull String node){
-        if(lang == null){
-            return null;
-        }
-        node = node.toLowerCase();
-        String[] nodes = node.split(".");
-        JsonObject obj = null;
-        for (String s : nodes) {
-            if (obj == null) {
-                obj = lang.get(s).getAsJsonObject();
-            } else {
-                obj = obj.get(s).getAsJsonObject();
+        try {
+            JsonObject obj = lang.get("item").getAsJsonObject().get("minecraft").getAsJsonObject();
+            String jsonName = obj.get(material.name().toLowerCase()).getAsString();
+            if (jsonName == null || jsonName.isEmpty()) {
+                return name;
             }
+            return jsonName;
+        } catch (NullPointerException npe) {
+            return name;
         }
-        if(obj == null){
+    }
+
+    @NotNull
+    public String getBlock(@NotNull Material material) {
+        String name = Util.prettifyText(material.name());
+        if (lang == null) {
+            return name;
+        }
+        try {
+            JsonObject obj = lang.get("block").getAsJsonObject().get("minecraft").getAsJsonObject();
+            String jsonName = obj.get(material.name().toLowerCase()).getAsString();
+            if (jsonName == null || jsonName.isEmpty()) {
+                return name;
+            }
+            return jsonName;
+        } catch (NullPointerException e) {
+            return name;
+        }
+    }
+
+    @NotNull
+    public String getEntity(@NotNull Material material) {
+        String name = Util.prettifyText(material.name());
+        if (lang == null) {
+            return name;
+        }
+        try {
+            JsonObject obj = lang.get("entity").getAsJsonObject().get("minecraft").getAsJsonObject();
+            String jsonName = obj.get(material.name().toLowerCase()).getAsString();
+            if (jsonName == null || jsonName.isEmpty()) {
+                return name;
+            }
+            return jsonName;
+        } catch (NullPointerException e) {
+            return name;
+        }
+    }
+
+    @NotNull
+    public String getPotion(@NotNull PotionEffectType effect) {
+        String name = Util.prettifyText(effect.getName());
+        if (lang == null) {
+            return name;
+        }
+        try {
+            JsonObject obj = lang.get("effect").getAsJsonObject().get("minecraft").getAsJsonObject();
+            String jsonName = obj.get(effect.getName().toLowerCase()).getAsString();
+            if (jsonName == null || jsonName.isEmpty()) {
+                return name;
+            }
+            return jsonName;
+        } catch (NullPointerException e) {
+            return name;
+        }
+    }
+
+    @NotNull
+    public String getEnchantment(@NotNull String enchantmentName) {
+        String name = Util.prettifyText(enchantmentName);
+        if (lang == null) {
+            return name;
+        }
+        try {
+            JsonObject obj = lang.get("enchantment").getAsJsonObject().get("minecraft").getAsJsonObject();
+            String jsonName = obj.get(enchantmentName.toLowerCase()).getAsString();
+            if (jsonName == null || jsonName.isEmpty()) {
+                return name;
+            }
+            return jsonName;
+        } catch (NullPointerException e) {
+            return name;
+        }
+    }
+
+    @Nullable
+    public String getCustom(@NotNull String node) {
+        if (lang == null) {
             return null;
         }
-        return obj.getAsString();
+        try {
+            node = node.toLowerCase();
+            String[] nodes = node.split(".");
+            JsonObject obj = null;
+            for (String s : nodes) {
+                if (obj == null) {
+                    obj = lang.get(s).getAsJsonObject();
+                } else {
+                    obj = obj.get(s).getAsJsonObject();
+                }
+            }
+            if (obj == null) {
+                return null;
+            }
+            return obj.getAsString();
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 }
