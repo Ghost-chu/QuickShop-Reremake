497c8492550586a9d465b295e0609e6a1ed6d485
cakoyo <i@omc.hk>
2020-02-01 06:11:39 +0700

    Fix import again
    I hate web editor

--- src/main/java/org/maxgamer/quickshop/Util/Updater.java
+++ src/main/java/org/maxgamer/quickshop/Util/Updater.java
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.Objects;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.InvalidDescriptionException;
