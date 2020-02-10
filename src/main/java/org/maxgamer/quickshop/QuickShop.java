83097de106d39b2a557e2f326b2672093193d208
portlek <hsndmrts_@outlook.com>
2020-02-10 13:26:18 +0300

    pom, plugin.yml and main class

---
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -0,0 +1,22 @@
+package org.maxgamer.quickshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuickShop extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
