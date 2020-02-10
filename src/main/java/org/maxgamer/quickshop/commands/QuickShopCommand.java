04042e67969c84c547ac866b94177d87dea458a4
portlek <hsndmrts_@outlook.com>
2020-02-10 17:51:10 +0300

    Update QuickShopCommand.java

--- src/main/java/org/maxgamer/quickshop/commands/QuickShopCommand.java
+++ src/main/java/org/maxgamer/quickshop/commands/QuickShopCommand.java
@@ -665,10 +665,7 @@
 package org.maxgamer.quickshop.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
 import io.github.portlek.configs.util.ListToString;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
@@ -677,9 +674,16 @@ import org.maxgamer.quickshop.QuickShopLoader;
 @CommandAlias("quickshop|qs")
 public final class QuickShopCommand extends BaseCommand {
 
+    @NotNull
+    private final QuickShopLoader loader;
+
+    public QuickShopCommand(@NotNull QuickShopLoader loader) {
+        this.loader = loader;
+    }
+
     @Default
     @CommandPermission("quickshop.command.main")
-    public void mainCommand(CommandSender sender, QuickShopLoader loader) {
+    public void mainCommand(CommandSender sender) {
         sender.sendMessage((String)
             loader.languageFile.help_messages.buildMap(list ->
                 new ListToString(list).value()
@@ -689,13 +693,13 @@ public final class QuickShopCommand extends BaseCommand {
 
     @Subcommand("help")
     @CommandPermission("quickshop.command.help")
-    public void helpCommand(CommandSender sender, QuickShopLoader loader) {
-        mainCommand(sender, loader);
+    public void helpCommand(CommandSender sender) {
+        mainCommand(sender);
     }
 
     @Subcommand("reload")
     @CommandPermission("quickshop.command.reload")
-    public void reloadCommand(CommandSender sender, QuickShopLoader loader) {
+    public void reloadCommand(CommandSender sender) {
         final long ms = System.currentTimeMillis();
 
         loader.reloadPlugin(false);
@@ -708,7 +712,7 @@ public final class QuickShopCommand extends BaseCommand {
 
     @Subcommand("version")
     @CommandPermission("quickshop.command.version")
-    public void versionCommand(CommandSender sender, QuickShopLoader loader) {
+    public void versionCommand(CommandSender sender) {
         loader.checkForUpdate(sender);
     }
 
