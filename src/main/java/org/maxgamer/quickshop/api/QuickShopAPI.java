e63e7b07d514e610280f662fa6fb12587ff660b9
portlek <hsndmrts_@outlook.com>
2020-02-10 14:11:29 +0300

    shop

--- src/main/java/org/maxgamer/quickshop/api/QuickShopAPI.java
+++ src/main/java/org/maxgamer/quickshop/api/QuickShopAPI.java
@@ -1,8 +1,20 @@
 package org.maxgamer.quickshop.api;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.maxgamer.quickshop.Shop;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public final class QuickShopAPI {
 
     private QuickShopAPI() {
+    }
+
+    @NotNull
+    public Optional<Shop> getByUUID(@NotNull UUID uuid) {
+        return Optional.empty();
     }
 
 }
