16cfdaf7a97fd0997d6e6247c6fbdd6428745431
portlek <hsndmrts_@outlook.com>
2020-02-10 14:50:20 +0300

    Update Shop.java

--- src/main/java/org/maxgamer/quickshop/Shop.java
+++ src/main/java/org/maxgamer/quickshop/Shop.java
@@ -1,7 +1,17 @@
 package org.maxgamer.quickshop;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
 public interface Shop {
 
-
+    /**
+     * Compares if given uuid is owner of the shop or not.
+     * 
+     * @param uuid the uuid that will compare
+     * @return returns true if the uuid is owner of the shop.
+     */
+    boolean isOwner(@NotNull UUID uuid);
 
 }
