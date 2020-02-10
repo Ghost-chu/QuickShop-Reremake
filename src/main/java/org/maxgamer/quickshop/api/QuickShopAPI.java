14f24f1c9a232ed48b836a3e8538edc0b32605bd
portlek <hsndmrts_@outlook.com>
2020-02-10 14:27:03 +0300

    Update QuickShopAPI.java

--- src/main/java/org/maxgamer/quickshop/api/QuickShopAPI.java
+++ src/main/java/org/maxgamer/quickshop/api/QuickShopAPI.java
@@ -3,6 +3,8 @@ package org.maxgamer.quickshop.api;
 import org.jetbrains.annotations.NotNull;
 import org.maxgamer.quickshop.Shop;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,6 +16,11 @@ public final class QuickShopAPI {
     @NotNull
     public Optional<Shop> getByUUID(@NotNull UUID uuid) {
         return Optional.empty();
+    }
+
+    @NotNull
+    public List<Shop> getShops() {
+        return Collections.emptyList();
     }
 
 }
