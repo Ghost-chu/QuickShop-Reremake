cfeb0b665b7e8ed7da566f62c149709fe633b01b
Hasan Demirtaş <utsukushihito@outlook.com>
2020-03-02 15:03:01 +0300

    Create ConfigFile.java
    Former-commit-id: afc60f15098534af6e25c9bed7d8f6dc45aad114

---
+++ src/main/java/org/maxgamer/quickshop/FilePortlek/newest/ConfigFile.java
@@ -0,0 +1,13 @@
+package org.maxgamer.quickshop.FilePortlek.newest;
+
+import io.github.portlek.configs.BukkitManaged;
+import io.github.portlek.configs.annotations.Config;
+import io.github.portlek.configs.util.FileType;
+
+@Config(
+    name = "config",
+    type = FileType.JSON,
+    location = "%basedir%/QuickShop"
+)
+public final class ConfigFile extends BukkitManaged {
+}
