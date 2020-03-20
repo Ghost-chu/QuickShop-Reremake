6bff1bc3a6ec23a3c45231b593be5c321573d4dc
sandtechnology <20417547+sandtechnology@users.noreply.github.com>
2020-03-20 21:21:06 +0800

    Revert "Fix a serious error that can delete all the shop"
    This reverts commit c2c39b47
    
    
    Former-commit-id: 03d6cf6618c0d30b7b0d5e75aca08b54f6bb1777

--- src/main/java/org/maxgamer/quickshop/Shop/ContainerShop.java
+++ src/main/java/org/maxgamer/quickshop/Shop/ContainerShop.java
@@ -28,7 +28,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.block.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -943,14 +945,16 @@ public class ContainerShop implements Shop {
             Util.debugLog(e.getMessage());
             return null;
         }
-            BlockState state=this.location.getBlock().getState();
-            if(state instanceof Container){
-                return ((Container) state).getInventory();
-            }else {
-                this.delete();
-                Util.debugLog("Inventory doesn't exist anymore: " + this + " shop was removed.");
-                return null;
-            }
+        InventoryHolder container;
+        try {
+            container = (InventoryHolder) this.location.getBlock().getState();
+            return container.getInventory();
+        } catch (Exception e) {
+            this.onUnload();
+            this.delete();
+            Util.debugLog("Inventory doesn't exist anymore: " + this + " shop was removed.");
+            return null;
+        }
     }
 
     /**
