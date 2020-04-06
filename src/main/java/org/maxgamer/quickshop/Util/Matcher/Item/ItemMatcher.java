d3c7b55630126cad6d43c99f1735e36aaa95b16e
Ghost_chu <2908803755@qq.com>
2020-04-06 12:44:53 +0800

    Designed for Interface and some impl
    Former-commit-id: 920188099a396818cd3fe90d03694a11b66a8797

---
+++ src/main/java/org/maxgamer/quickshop/Util/Matcher/Item/ItemMatcher.java
@@ -0,0 +1,28 @@
+package org.maxgamer.quickshop.Util.Matcher.Item;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public interface ItemMatcher {
+    /**
+     * Gets the ItemMatcher provider name
+     * @return Provider name
+     */
+    @NotNull String getName();
+
+    /**
+     * Gets the ItemMatcher provider plugin instance
+     * @return Provider Plugin instance
+     */
+    @NotNull Plugin getPlugin();
+
+    /**
+     * Tests ItemStacks is matches
+     * ->BEWARE: Different order of itemstacks you might will got different results<-
+     * @param original The original ItemStack
+     * @param tester The ItemStack will test matches with original itemstack.
+     * @return The result of tests
+     */
+    boolean matches(@NotNull ItemStack original, @NotNull ItemStack tester);
+}
