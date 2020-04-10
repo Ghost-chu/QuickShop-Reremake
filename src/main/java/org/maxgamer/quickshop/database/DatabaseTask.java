4329cfee191267c895f479998fe3b01809c6e5fe
Ghost_chu <2908803755@qq.com>
2020-04-10 13:39:10 +0800

    Dumb
    Former-commit-id: 77bbe0845ba1b69e1739f3a6e5ec08928e233536

--- src/main/java/org/maxgamer/quickshop/database/DatabaseTask.java
+++ src/main/java/org/maxgamer/quickshop/database/DatabaseTask.java
@@ -31,7 +31,7 @@ public class DatabaseTask {
         }
     }
     public void run(@NonNull Connection connection) {
-        try (PreparedStatement ps = connection.prepareStatement(statement);) {
+        try (PreparedStatement ps = connection.prepareStatement(statement)) {
             task.edit(ps);
             ps.execute();
             task.onSuccess();
