c4b4e500a02b1f7259590cb5969c4a141bcfa061
sandtechnology <a1294790523@hotmail.com>
2020-07-12 13:11:15 +0800

    Add javadoc and new method for IntegratedPlugin
    Former-commit-id: 3c99b216226fc43657e61f1e32e503a46795bdf9

--- src/main/java/org/maxgamer/quickshop/integration/IntegratedPlugin.java
+++ src/main/java/org/maxgamer/quickshop/integration/IntegratedPlugin.java
@@ -23,16 +23,57 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+
+@IntegrationStage
 public interface IntegratedPlugin {
+    /**
+     * Return the integrated plugin name.
+     * For example, Residence
+     *
+     * @return integrated plugin
+     */
     @NotNull
     String getName();
 
+    /**
+     * Check if a player can create shop here
+     *
+     * @param player   the player want to create shop
+     * @param location shop location
+     * @return If you can create shop here
+     */
     boolean canCreateShopHere(@NotNull Player player, @NotNull Location location);
 
+    /**
+     * Check if a player can trade with shop here
+     *
+     * @param player   the player want to trade with shop
+     * @param location shop location
+     * @return If you can trade with shop here
+     */
     boolean canTradeShopHere(@NotNull Player player, @NotNull Location location);
 
+    /**
+     * Check if a player can delete a shop here
+     *
+     * @param player   the player want to delete the shop
+     * @param location shop location
+     * @return If you can delete the shop here
+     */
+    default boolean canDeleteShopHere(@NotNull Player player, @NotNull Location location) {
+        return false;
+    }
+
+    /**
+     * Loading logic
+     * Execute Stage defined by IntegrationStage
+     */
     void load();
 
+    /**
+     * Unloding logic
+     * Will execute when Quickshop unloading
+     */
     void unload();
 
 }
