6547be8913f204cb8701ccec54e12ca69e4f1ad0
Ghost_chu <2908803755@qq.com>
2020-08-12 20:55:44 +0800

    Fix the "delete(true)" and disallow ShopLoader to delete the shop

--- src/main/java/org/maxgamer/quickshop/database/DatabaseHelper.java
+++ src/main/java/org/maxgamer/quickshop/database/DatabaseHelper.java
@@ -182,7 +182,13 @@ public class DatabaseHelper {
                 ps.setInt(4, location.getBlockX());
                 ps.setInt(5, location.getBlockY());
                 ps.setInt(6, location.getBlockZ());
-                ps.setString(7, location.getWorld().getName());
+                String worldName = "undefined";
+                if (location.getWorld() != null) {
+                    worldName = location.getWorld().getName();
+                } else {
+                    plugin.getLogger().warning("Warning: Shop " + shop + " had null world name due we will save it as undefined world to trying keep data.");
+                }
+                ps.setString(7, worldName);
                 ps.setInt(8, shop.isUnlimited() ? 1 : 0);
                 ps.setInt(9, shop.getShopType().toID());
                 ps.setString(10, shop.saveExtraToJson());
