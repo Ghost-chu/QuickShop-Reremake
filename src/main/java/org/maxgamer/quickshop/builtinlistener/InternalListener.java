992fb1a86718aec6240122db9bf14e2ae34e5065
sandtechnology <a1294790523@hotmail.com>
2020-09-07 15:28:38 +0800

    Another optimization 1.Reduce calling getBalance and format method 2.Fix time bug in qs.log 3.Re-work logging option

--- src/main/java/org/maxgamer/quickshop/builtinlistener/InternalListener.java
+++ src/main/java/org/maxgamer/quickshop/builtinlistener/InternalListener.java
@@ -20,7 +20,6 @@
 
 package org.maxgamer.quickshop.builtinlistener;
 
-import lombok.AllArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -29,91 +28,113 @@ import org.maxgamer.quickshop.event.*;
 import org.maxgamer.quickshop.shop.ShopType;
 import org.maxgamer.quickshop.util.Util;
 
-@AllArgsConstructor
+
 public class InternalListener implements Listener {
     private final QuickShop plugin;
+    private final boolean loggingBalance;
+    private final boolean loggingAction;
+
+    public InternalListener(QuickShop plugin) {
+        this.plugin = plugin;
+        this.loggingBalance = plugin.getConfig().getBoolean("logging.log-balance");
+        this.loggingAction = plugin.getConfig().getBoolean("logging.log-actions");
+    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopCreate(ShopCreateEvent event) {
-        plugin.log(
-                "Player "
-                        + event.getPlayer().getName()
-                        + " created a shop at location "
-                        + event.getShop().getLocation());
+        if (loggingAction) {
+            plugin.log(
+                    "Player "
+                            + event.getPlayer().getName()
+                            + " created a shop at location "
+                            + event.getShop().getLocation());
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopDelete(ShopDeleteEvent event) {
-        plugin.log("Shop at " + event.getShop().getLocation() + " was removed.");
+        if (loggingAction) {
+            plugin.log("Shop at " + event.getShop().getLocation() + " was removed.");
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopModeratorChanges(ShopModeratorChangedEvent event) {
-        plugin.log(
-                "Shop at location "
-                        + event.getShop().getLocation()
-                        + " moderator was changed to "
-                        + event.getModerator());
+        if (loggingAction) {
+            plugin.log(
+                    "Shop at location "
+                            + event.getShop().getLocation()
+                            + " moderator was changed to "
+                            + event.getModerator());
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPriceChanges(ShopPriceChangeEvent event) {
-        plugin.log(
-                "Shop at location "
-                        + event.getShop().getLocation()
-                        + " price was changed from "
-                        + event.getOldPrice()
-                        + " to "
-                        + event.getNewPrice());
+        if (loggingAction) {
+            plugin.log(
+                    "Shop at location "
+                            + event.getShop().getLocation()
+                            + " price was changed from "
+                            + event.getOldPrice()
+                            + " to "
+                            + event.getNewPrice());
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPrePurchase(ShopPurchaseEvent event) {
-        plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " before trading.");
-        plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " before trading.");
+        if (loggingBalance) {
+            plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " before trading.");
+            plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " before trading.");
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPurchase(ShopSuccessPurchaseEvent event) {
-        if (event.getShop().getShopType() == ShopType.BUYING) {
-            plugin.log(
-                    "Player "
-                            + event.getPlayer().getName()
-                            + " sold "
-                            + event.getShop().ownerName()
-                            + " shop "
-                            + event.getShop()
-                            + " for"
-                            + Util.getItemStackName(event.getShop().getItem())
-                            + "x" +
-                            +event.getAmount()
-                            + " for "
-                            + plugin.getEconomy().format(event.getBalance())
-                            + " ("
-                            + plugin.getEconomy().format(event.getTax())
-                            + " tax).");
-        }
-        if (event.getShop().getShopType() == ShopType.SELLING) {
-            plugin.log(
-                    "Player "
-                            + event.getPlayer().getName()
-                            + " bought "
-                            + event.getShop().ownerName()
-                            + " shop "
-                            + event.getShop()
-                            + " for "
-                            + Util.getItemStackName(event.getShop().getItem())
-                            + " x"
-                            + event.getAmount()
-                            + " for "
-                            + plugin.getEconomy().format(event.getBalance())
-                            + " ("
-                            + plugin.getEconomy().format(event.getTax())
-                            + " tax).");
+        if (loggingAction) {
+            if (event.getShop().getShopType() == ShopType.BUYING) {
+                plugin.log(
+                        "Player "
+                                + event.getPlayer().getName()
+                                + " sold "
+                                + event.getShop().ownerName()
+                                + " shop "
+                                + event.getShop()
+                                + " for"
+                                + Util.getItemStackName(event.getShop().getItem())
+                                + "x" +
+                                +event.getAmount()
+                                + " for "
+                                + event.getBalance()
+                                + " ("
+                                + event.getTax()
+                                + " tax).");
+            }
+            if (event.getShop().getShopType() == ShopType.SELLING) {
+                plugin.log(
+                        "Player "
+                                + event.getPlayer().getName()
+                                + " bought "
+                                + event.getShop().ownerName()
+                                + " shop "
+                                + event.getShop()
+                                + " for "
+                                + Util.getItemStackName(event.getShop().getItem())
+                                + " x"
+                                + event.getAmount()
+                                + " for "
+                                + event.getBalance()
+                                + " ("
+                                + event.getTax()
+                                + " tax).");
 
+            }
         }
-        plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " after trading.");
-        plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " after trading.");
+        if (loggingBalance) {
+            plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " after trading.");
+            plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " after trading.");
+        }
     }
 
 }
