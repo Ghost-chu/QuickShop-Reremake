5c87c2c8dcaf0b53c3073ce6c93ef8a926626b66
Ghost_chu <2908803755@qq.com>
2020-09-29 17:13:21 +0800

    No-longer use Github API
    Former-commit-id: fceac81c589f2c7d683bee76f3348850bd2c0299

--- src/main/java/org/maxgamer/quickshop/util/github/GithubAPI.java
+++ src/main/java/org/maxgamer/quickshop/util/github/GithubAPI.java
@@ -19,46 +19,46 @@
 
 package org.maxgamer.quickshop.util.github;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.jetbrains.annotations.Nullable;
-import org.maxgamer.quickshop.nonquickshopstuff.com.sk89q.worldedit.util.net.HttpRequest;
-
-import java.io.IOException;
-import java.net.URL;
-
-public class GithubAPI {
-    private final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-
-    @Nullable
-    public ReleaseJsonContainer.AssetsBean getLatestRelease() throws IOException {
-        String json =
-                HttpRequest.get(
-                        new URL(
-                                "https://api.github.com/repos/Ghost-chu/QuickShop-Reremake/releases/latest"))
-                        .execute()
-                        .returnContent()
-                        .asString("UTF-8");
-        ReleaseJsonContainer result = gson.fromJson(json, ReleaseJsonContainer.class);
-        for (ReleaseJsonContainer.AssetsBean asset : result.getAssets()) {
-            if (asset.getName().contains("original-")) {
-                continue;
-            }
-            if (asset.getName().contains("-javadoc")) {
-                continue;
-            }
-            if (asset.getName().contains("-sources")) {
-                continue;
-            }
-            if (asset.getName().contains("-shaded")) {
-                continue;
-            }
-            if (asset.getName().contains("Document") || asset.getName().contains("document")) {
-                continue;
-            }
-            return asset;
-        }
-        return null;
-    }
-
-}
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//import org.jetbrains.annotations.Nullable;
+//import org.maxgamer.quickshop.nonquickshopstuff.com.sk89q.worldedit.util.net.HttpRequest;
+//
+//import java.io.IOException;
+//import java.net.URL;
+//
+//public class GithubAPI {
+//    private final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+//
+//    @Nullable
+//    public ReleaseJsonContainer.AssetsBean getLatestRelease() throws IOException {
+//        String json =
+//                HttpRequest.get(
+//                        new URL(
+//                                "https://api.github.com/repos/Ghost-chu/QuickShop-Reremake/releases/latest"))
+//                        .execute()
+//                        .returnContent()
+//                        .asString("UTF-8");
+//        ReleaseJsonContainer result = gson.fromJson(json, ReleaseJsonContainer.class);
+//        for (ReleaseJsonContainer.AssetsBean asset : result.getAssets()) {
+//            if (asset.getName().contains("original-")) {
+//                continue;
+//            }
+//            if (asset.getName().contains("-javadoc")) {
+//                continue;
+//            }
+//            if (asset.getName().contains("-sources")) {
+//                continue;
+//            }
+//            if (asset.getName().contains("-shaded")) {
+//                continue;
+//            }
+//            if (asset.getName().contains("Document") || asset.getName().contains("document")) {
+//                continue;
+//            }
+//            return asset;
+//        }
+//        return null;
+//    }
+//
+//}
