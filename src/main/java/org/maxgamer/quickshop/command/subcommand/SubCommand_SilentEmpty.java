16831b5de296a050472d45d16f2f79c47a9ed33a
Ghost_chu <2908803755@qq.com>
2020-10-01 13:37:24 +0800

    UUID based shop control panel system and caching
    Former-commit-id: 5bbf06a3e28aafc5fe6b36db5ff82b58e58c4a31

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentEmpty.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentEmpty.java
@@ -1,6 +1,5 @@
 /*
  * This file is a part of project QuickShop, the name is SubCommand_SilentEmpty.java
- *  Copyright (C) Ghost_chu <https://github.com/Ghost-chu>
  *  Copyright (C) PotatoCraft Studio and contributors
  *
  *  This program is free software: you can redistribute it and/or modify it
@@ -41,20 +40,26 @@ public class SubCommand_SilentEmpty implements CommandProcesser {
     @Override
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
-        if (cmdArg.length < 4) {
+        if (cmdArg.length < 1) {
             Util.debugLog("Exception on command, cancel.");
             return;
         }
 
-        final Shop shop =
-                plugin
-                        .getShopManager()
-                        .getShop(
-                                new Location(
-                                        ServerUtil.fromWorldNameID(cmdArg[0]),
-                                        Integer.parseInt(cmdArg[1]),
-                                        Integer.parseInt(cmdArg[2]),
-                                        Integer.parseInt(cmdArg[3])));
+        Shop shop = plugin.getShopManager().getShopFromRuntimeRandomUniqueId(UUID.fromString(cmdArg[0]));
+//        if (cmdArg.length < 1) {
+//            Util.debugLog("Exception on command, cancel.");
+//            return;
+//        }
+//
+//        final Shop shop =
+//                plugin
+//                        .getShopManager()
+//                        .getShop(
+//                                new Location(
+//                                        plugin.getServer().getWorld(cmdArg[0]),
+//                                        Integer.parseInt(cmdArg[1]),
+//                                        Integer.parseInt(cmdArg[2]),
+//                                        Integer.parseInt(cmdArg[3])));
 
         if (!(shop instanceof ContainerShop)) {
             MsgUtil.sendMessage(sender, MsgUtil.getMessage("not-looking-at-shop", sender));
--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentRemove.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentRemove.java
@@ -1,6 +1,5 @@
 /*
  * This file is a part of project QuickShop, the name is SubCommand_SilentRemove.java
- *  Copyright (C) Ghost_chu <https://github.com/Ghost-chu>
  *  Copyright (C) PotatoCraft Studio and contributors
  *
  *  This program is free software: you can redistribute it and/or modify it
@@ -22,7 +21,6 @@ package org.maxgamer.quickshop.command.subcommand;
 
 import lombok.AllArgsConstructor;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -30,7 +28,9 @@ import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.command.CommandProcesser;
 import org.maxgamer.quickshop.shop.Shop;
 import org.maxgamer.quickshop.util.MsgUtil;
-import org.maxgamer.quickshop.util.ServerUtil;
+import org.maxgamer.quickshop.util.Util;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 public class SubCommand_SilentRemove implements CommandProcesser {
@@ -40,20 +40,27 @@ public class SubCommand_SilentRemove implements CommandProcesser {
     @Override
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
-        if (cmdArg.length < 4) {
+
+        if (cmdArg.length < 1) {
+            Util.debugLog("Exception on command, cancel.");
             return;
         }
 
+        Shop shop = plugin.getShopManager().getShopFromRuntimeRandomUniqueId(UUID.fromString(cmdArg[0]));
+//        if (cmdArg.length < 4) {
+//            return;
+//        }
+
         final Player p = (Player) sender;
-        final Shop shop =
-                plugin
-                        .getShopManager()
-                        .getShop(
-                                new Location(
-                                        ServerUtil.fromWorldNameID(cmdArg[0]),
-                                        Integer.parseInt(cmdArg[1]),
-                                        Integer.parseInt(cmdArg[2]),
-                                        Integer.parseInt(cmdArg[3])));
+//        final Shop shop =
+//                plugin
+//                        .getShopManager()
+//                        .getShop(
+//                                new Location(
+//                                        plugin.getServer().getWorld(cmdArg[0]),
+//                                        Integer.parseInt(cmdArg[1]),
+//                                        Integer.parseInt(cmdArg[2]),
+//                                        Integer.parseInt(cmdArg[3])));
 
         if (shop == null) {
             MsgUtil.sendMessage(sender, MsgUtil.getMessage("not-looking-at-shop", sender));
