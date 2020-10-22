8a5f39890f2e7f5ad631bdf239886b9732b8c8e2
sandtechnology <a1294790523@hotmail.com>
2020-10-22 20:51:49 +0800

    Add LICENSE and document
    Former-commit-id: 61bc32c293afa62355eccb6255ec90d5087e2f03

--- src/main/java/org/maxgamer/quickshop/util/InteractUtil.java
+++ src/main/java/org/maxgamer/quickshop/util/InteractUtil.java
@@ -1,3 +1,21 @@
+/*
+ * This file is a part of project QuickShop, the name is Bootstrap.java
+ *  Copyright (C) PotatoCraft Studio and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.maxgamer.quickshop.util;
 
 import org.bukkit.configuration.file.FileConfiguration;
@@ -5,6 +23,12 @@ import org.maxgamer.quickshop.QuickShop;
 
 import java.util.EnumMap;
 
+/***
+ * Util class for interacting with shop
+ *
+ * @author sandtechnology
+ * @since 4.0.5.0
+ */
 public class InteractUtil {
     private final static EnumMap<Action, Boolean> sneakingActionMap = new EnumMap<>(Action.class);
     private static Mode mode;
@@ -19,6 +43,13 @@ public class InteractUtil {
         init = true;
     }
 
+    /**
+     * Check if can interact with shop
+     *
+     * @param action     the action taking
+     * @param isSneaking is the player sneaking
+     * @return if can interact with shop
+     */
     public static boolean check(Action action, boolean isSneaking) {
         if (!init) {
             init();
@@ -40,6 +71,12 @@ public class InteractUtil {
     public enum Mode {
         ONLY, BOTH, REVERSED;
 
+        /**
+         * Return the mode by int value
+         *
+         * @param mode int value
+         * @return the mode by int value, return ONLY when out of bounds
+         */
         public static Mode getMode(int mode) {
             return Mode.values().length > mode ? Mode.values()[mode] : ONLY;
         }
