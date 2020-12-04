66d6727491e8643ce5567768ebddf9d865a463fd
Ghost_chu <2908803755@qq.com>
2020-12-04 13:42:53 +0800

    Revert "Fix build error"
    This reverts commit ee89a3b0
    
    
    Former-commit-id: c11a3d02944021698ffeccb7353d16c0db8b8fa9

--- src/main/java/org/maxgamer/quickshop/builtinlistener/InternalListener.java
+++ src/main/java/org/maxgamer/quickshop/builtinlistener/InternalListener.java
@@ -19,8 +19,6 @@
 
 package org.maxgamer.quickshop.builtinlistener;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -44,10 +42,9 @@ public class InternalListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopCreate(ShopCreateEvent event) {
         if (loggingAction) {
-            Player creator = Bukkit.getPlayer(event.getCreator());
             plugin.log(
                     "Player "
-                            + (creator != null ? creator.getName() : event.getCreator())
+                            + event.getPlayer().getName()
                             + " created a shop at location "
                             + event.getShop().getLocation());
         }
@@ -87,20 +84,18 @@ public class InternalListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPrePurchase(ShopPurchaseEvent event) {
         if (loggingBalance) {
-            Player creator = Bukkit.getPlayer(event.getPurchaser());
-            plugin.log("Player " + (creator != null ? creator.getName() : event.getPurchaser()) + " had " + plugin.getEconomy().getBalance(event.getPurchaser()) + " before trading.");
+            plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " before trading.");
             plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " before trading.");
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void shopPurchase(ShopSuccessPurchaseEvent event) {
-        Player creator = Bukkit.getPlayer(event.getPurchaser());
         if (loggingAction) {
             if (event.getShop().getShopType() == ShopType.BUYING) {
                 plugin.log(
                         "Player "
-                                + (creator != null ? creator.getName() : event.getPurchaser())
+                                + event.getPlayer().getName()
                                 + " sold "
                                 + event.getShop().ownerName()
                                 + " shop "
@@ -118,7 +113,7 @@ public class InternalListener implements Listener {
             if (event.getShop().getShopType() == ShopType.SELLING) {
                 plugin.log(
                         "Player "
-                                + (creator != null ? creator.getName() : event.getPurchaser())
+                                + event.getPlayer().getName()
                                 + " bought "
                                 + event.getShop().ownerName()
                                 + " shop "
@@ -136,7 +131,7 @@ public class InternalListener implements Listener {
             }
         }
         if (loggingBalance) {
-            plugin.log("Player " + (creator != null ? creator.getName() : event.getPurchaser()) + " had " + plugin.getEconomy().getBalance(event.getPurchaser()) + " after trading.");
+            plugin.log("Player " + event.getPlayer().getName() + " had " + plugin.getEconomy().getBalance(event.getPlayer().getUniqueId()) + " after trading.");
             plugin.log("Shop Owner " + event.getShop().ownerName() + " had " + plugin.getEconomy().getBalance(event.getShop().getOwner()) + " after trading.");
         }
     }
