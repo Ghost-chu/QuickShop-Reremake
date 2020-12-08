3cde763fdebf5cbc1dd9c40702699ca998b33f38
sandtechnology <a1294790523@hotmail.com>
2020-12-08 14:10:31 +0800

    Fix a resources leak in database

--- src/main/java/org/maxgamer/quickshop/database/WarpedResultSet.java
+++ src/main/java/org/maxgamer/quickshop/database/WarpedResultSet.java
@@ -4,13 +4,16 @@ import lombok.Getter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class WarpedResultSet implements AutoCloseable {
     @Getter
     private final ResultSet resultSet;
     private final DatabaseConnection databaseConnection;
+    private final Statement statement;
 
-    public WarpedResultSet(ResultSet resultSet, DatabaseConnection databaseConnection) {
+    public WarpedResultSet(Statement statement, ResultSet resultSet, DatabaseConnection databaseConnection) {
+        this.statement = statement;
         this.resultSet = resultSet;
         this.databaseConnection = databaseConnection;
     }
@@ -18,6 +21,7 @@ public class WarpedResultSet implements AutoCloseable {
     @Override
     public void close() throws SQLException {
         resultSet.close();
+        statement.close();
         databaseConnection.release();
     }
 }
