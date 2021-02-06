7eb003d42c92a9045ecdff0828b3dafa927c9e9b
Ghost_chu <2908803755@qq.com>
2021-02-06 23:24:17 +0800

    [WIP] Rewritten MojangAPI utils, unfinished

--- src/main/java/org/maxgamer/quickshop/util/mojangapi/MojangAPI.java
+++ src/main/java/org/maxgamer/quickshop/util/mojangapi/MojangAPI.java
@@ -19,118 +19,176 @@
 
 package org.maxgamer.quickshop.util.mojangapi;
 
-import com.google.common.io.Files;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
+import lombok.Data;
+import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.QuickShop;
-import org.maxgamer.quickshop.nonquickshopstuff.com.sk89q.worldedit.util.net.HttpRequest;
-import org.maxgamer.quickshop.util.JsonUtil;
-import org.maxgamer.quickshop.util.Util;
+import org.maxgamer.quickshop.util.HttpCacheLoader;
 
-import java.io.File;
-import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class MojangAPI {
-    private static final String versionManifestUrl = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
-
-    private static final String assetsUrl = "https://resources.download.minecraft.net/";
-
-    //private static final String pathTemplate = "minecraft/lang/{0}.json";
-
-    private static final Gson gson = JsonUtil.getGson();
-
     private final QuickShop plugin;
 
-    public MojangAPI(@NotNull QuickShop plugin) {
+
+    public MojangAPI(QuickShop plugin) {
         this.plugin = plugin;
     }
 
-    @Nullable
-    public String getAssetIndexJson(@NotNull String mcVer) throws IOException {
-        String versionJson = getVersionJson(mcVer);
-        if (versionJson == null) {
-            return null;
-        }
-        JsonObject rootObj = new JsonParser().parse(versionJson).getAsJsonObject();
-        JsonObject assetIndex = rootObj.getAsJsonObject("assetIndex");
-        if (assetIndex == null) {
-            Util.debugLog("Cannot get assetIndex obj.");
-            return null;
-        }
-        JsonPrimitive urlObj = assetIndex.getAsJsonPrimitive("url");
-        if (urlObj == null) {
-            Util.debugLog("Cannot get asset url obj.");
-            return null;
-        }
-        String url = urlObj.getAsString();
-        if (url == null) {
-            Util.debugLog("Cannot get asset url.");
-            return null;
-        }
-        return HttpRequest.get(new URL(url))
-                .execute()
-                .expectResponseCode(200)
-                .returnContent()
-                .asString("UTF-8")
-                .trim();
+    @NotNull
+    public AssetsAPI getAssetsAPI(@NotNull String serverVersion) {
+        return new AssetsAPI(serverVersion);
     }
 
-    @Nullable
-    public String getVersionJson(@NotNull String mcVer) throws IOException {
-        VersionList list = gson.fromJson(this.getVersionManifest(), VersionList.class);
-        for (VersionList.VersionsBean mcv : list.getVersions()) {
-            if (mcv.getId().equals(mcVer)) {
-                try {
-                    plugin.getLogger().info("Downloading version index...");
-                    return HttpRequest.get(new URL(mcv.getUrl()))
-                            .execute()
-                            .expectResponseCode(200)
-                            .returnContent()
-                            .asString("UTF-8")
-                            .trim();
-                } catch (IOException e) {
-                    Util.debugLog(e.getMessage());
-                    return null;
+    static class AssetsAPI {
+        private final LoadingCache<URL, Optional<String>> request = CacheBuilder.newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(new HttpCacheLoader());
+        private final MetaAPI metaAPI;
+
+        AssetsAPI(@NotNull String version) {
+            this.metaAPI = new MetaAPI(version);
+        }
+
+        public boolean isAvailable() {
+            return this.metaAPI.get().isPresent();
+        }
+
+        /**
+         * Gets the GameAsset file content
+         *
+         * @param langCode LanguageCode
+         * @return The file content
+         */
+        public Optional<String> getGameAssetsFile(@NotNull String langCode) {
+            Optional<GameInfoAPI.DataBean> bean = getAssetsJson();
+            if (!bean.isPresent())
+                return Optional.empty();
+            GameInfoAPI.DataBean.AssetIndexBean assetIndexBean = bean.get().getAssetIndex();
+            if (assetIndexBean == null || assetIndexBean.getUrl() == null || assetIndexBean.getId() == null)
+                return Optional.empty();
+
+            try {
+                return request.get(new URL(assetIndexBean.getUrl()));
+            } catch (ExecutionException | MalformedURLException e) {
+                return Optional.empty();
+            }
+
+        }
+
+
+        private Optional<GameInfoAPI.DataBean> getAssetsJson() {
+            if (!isAvailable())
+                return Optional.empty();
+            Optional<String> content = this.metaAPI.get();
+            if (!content.isPresent())
+                return Optional.empty();
+            GameInfoAPI gameInfoAPI = new GameInfoAPI(content.get());
+            return Optional.of(gameInfoAPI.get());
+        }
+
+
+    }
+
+    @Data
+    static class GameInfoAPI {
+        private final String json;
+        private final Gson gson = new Gson();
+
+        GameInfoAPI(@NotNull String json) {
+            this.json = json;
+        }
+
+        @NotNull
+        public DataBean get() {
+            return gson.fromJson(json, DataBean.class);
+        }
+
+        @Data
+        static class DataBean {
+            @Nullable
+            private AssetIndexBean assetIndex;
+            @Nullable
+            private String assets;
+
+            @Data
+            public static class AssetIndexBean {
+                /**
+                 * id : 1.16
+                 * sha1 : 3a5d110a6ab102c7083bae4296d2de4b8fcf92eb
+                 * size : 295421
+                 * totalSize : 330604420
+                 * url : https://launchermeta.mojang.com/v1/packages/3a5d110a6ab102c7083bae4296d2de4b8fcf92eb/1.16.json
+                 */
+                @Nullable
+                private String id;
+                @Nullable
+                private String sha1;
+                @Nullable
+                private String url;
+            }
+
+        }
+
+
+    }
+
+    static class MetaAPI {
+        //Cache with URL and Content(String)
+        private final LoadingCache<URL, Optional<String>> request = CacheBuilder.newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .build(new HttpCacheLoader());
+        private final URL metaEndpoint;
+        private final String version;
+
+        @SneakyThrows
+        public MetaAPI(@NotNull String version) {
+            this.version = version;
+            this.metaEndpoint = new URL("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+        }
+
+        /**
+         * Gets the available status and the Game Version Meta Json File content.
+         *
+         * @return The meta data
+         */
+        @SneakyThrows
+        public Optional<String> get() {
+            Optional<String> result = request.get(this.metaEndpoint);
+            if (!result.isPresent())
+                return Optional.empty();
+            try {
+                JsonElement index = new JsonParser().parse(result.get());
+                if (!index.isJsonObject())
+                    return Optional.empty();
+                JsonElement availableVersions = index.getAsJsonObject().get("versions");
+                if (!availableVersions.isJsonArray())
+                    return Optional.empty();
+                for (JsonElement gameVersionData : availableVersions.getAsJsonArray()) {
+                    if (gameVersionData.isJsonObject()) {
+                        JsonElement gameId = gameVersionData.getAsJsonObject().get("id");
+                        JsonElement gameIndexUrl = gameVersionData.getAsJsonObject().get("url");
+                        if (Objects.equals(gameId.getAsString(), version))
+                            return request.get(new URL(gameIndexUrl.getAsString()));
+                    }
                 }
+                return Optional.empty();
+            } catch (RuntimeException exception) {
+                return Optional.empty();
             }
         }
-        return null;
     }
 
-    @Nullable
-    public String getVersionManifest() throws IOException {
-        plugin.getLogger().info("Downloading version manifest...");
-        return HttpRequest.get(new URL(versionManifestUrl))
-                .execute()
-                .expectResponseCode(200)
-                .returnContent()
-                .asString("UTF-8")
-                .trim();
-    }
-
-    @Nullable
-    public String downloadTextFileFromMojang(@NotNull String hash) throws IOException {
-        File cacheFile = new File(Util.getCacheFolder(), hash);
-        if (cacheFile.exists()) {
-            return Util.readToString(cacheFile);
-        }
-        String data;
-        plugin.getLogger().info("Downloading assets file...");
-        data =
-                HttpRequest.get(new URL(assetsUrl + hash.substring(0, 2) + "/" + hash))
-                        .execute()
-                        .expectResponseCode(200)
-                        .returnContent()
-                        .asString("UTF-8")
-                        .trim();
-        Files.write(data.getBytes(StandardCharsets.UTF_8), cacheFile);
-        return data;
-    }
 
 }
