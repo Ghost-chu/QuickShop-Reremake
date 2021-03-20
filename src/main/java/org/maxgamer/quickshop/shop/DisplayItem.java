c58022a3875092c6aab5e4a0cab4ddd66797e8fa
Star <iamstarmism@gmail.com>
2021-03-20 06:31:34 -0600

    Address Issue #807 (Only use a single item and center it for a double chest buy/sell shop.) (#872)
    * Expanded duplicate checking to check both the spots that a double chest would spawn one normally, to eliminate double items for double chests.
    
    * Addressed issue #807 (Only use a single item and center it for a double chest buy/sell shop.)
    
    * Add a few superclass methods required for single item per doublechest.
    
    * Fixed different items not being allowed to be in the same doubleshop chest.
    
    * Added isRealDouble() check to isLeftShop() since they're always used together and it is easy to forget.
    
    * Fixed the logic for the isSpawned check, since it would always return true if it had a left shop item, which could lead to missing displayItems.
    
    * Fixed direction checking (kinda important)
    
    * Cleanup
    
    * Actually implementing the change for Virtual items
    
    * Silence the warning and stop checking for non-existant stuff
    
    * Refresh it's partner if it's a double and refresh itself at the very end.
    
    * Moved common behaviour to the superclass for code consolidation
    
    * Expanded the double ternany statement because it was confusing
    
    * Cached the results of getAttachedChest() and isLeftShop() for quicker use
    
    * Add attempt to update attached item, to ensure on spawn there are no duplicates.
    
    * Add/move Javadocs
    
    * Complying with String formatting
    
    * Cleanup. Add comments, rename variables, remove debug messages, etc.
    
    * Unnecessary. Forgot to remove in last wave of commits :/
    
    * Add default branch for safety!

--- src/main/java/org/maxgamer/quickshop/shop/DisplayItem.java
+++ src/main/java/org/maxgamer/quickshop/shop/DisplayItem.java
@@ -136,6 +136,10 @@ public abstract class DisplayItem {
         }
         String defaultMark = ShopProtectionFlag.getDefaultMark();
         String shopLocation = shop.getLocation().toString();
+        String attachedShopLocation = "null";
+        if (shop.isRealDouble()) {
+            attachedShopLocation = shop.getAttachedShop().getLocation().toString();
+        }
         //noinspection ConstantConditions
         for (String lore : iMeta.getLore()) {
             try {
@@ -149,7 +153,8 @@ public abstract class DisplayItem {
                 if (!ShopProtectionFlag.getMark().equals(defaultMark)) {
                     continue;
                 }
-                if (shopProtectionFlag.getShopLocation().equals(shopLocation)) {
+                if (shopProtectionFlag.getShopLocation().equals(shopLocation)
+                || shopProtectionFlag.getShopLocation().equals(attachedShopLocation)) {
                     return true;
                 }
             } catch (JsonSyntaxException e) {
@@ -297,11 +302,27 @@ public abstract class DisplayItem {
     public abstract Entity getDisplay();
 
     /**
-     * Get display should at location. Not display current location.
+     * Gets the display location for an item. If it is a double shop and it is not the left shop,
+     * it will average the locations of the two chests comprising it to be perfectly in the middle.
+     * If it is the left shop, it will return null since the left shop does not spawn an item.
+     * Otherwise, it will give you the middle of the single chest.
      *
-     * @return Should at
+     * @return The Location that the item *should* be displaying at.
      */
-    public abstract Location getDisplayLocation();
+    public @Nullable Location getDisplayLocation() {
+        Util.ensureThread(false);
+        if (shop.isRealDouble()) {
+            if (shop.isLeftShop()) {
+                Util.debugLog("Shop is left shop, so location is null.");
+                return null;
+            }
+            double avgX = (shop.getLocation().getX() + shop.getAttachedShop().getLocation().getX()) / 2;
+            double avgZ = (shop.getLocation().getZ() + shop.getAttachedShop().getLocation().getZ()) / 2;
+            Location newloc = new Location(shop.getLocation().getWorld(), avgX, shop.getLocation().getY(), avgZ, shop.getLocation().getYaw(), shop.getLocation().getPitch());
+            return newloc.add(0.5, 1.2, 0.5);
+        }
+        return this.shop.getLocation().clone().add(0.5, 1.2, 0.5);
+    }
 
     /**
      * Check the display is or not already spawned
