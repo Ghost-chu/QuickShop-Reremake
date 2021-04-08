ec9cd02c4761c40012bb9b2b1e9bfbd1b41bf3b5
Ghost_chu <2908803755@qq.com>
2021-04-08 16:33:18 +0800

    Stop using portlek files and migrate to JsonConfiguration

--- src/main/java/org/maxgamer/quickshop/fileportlek/old/YAMLFile.java
+++ src/main/java/org/maxgamer/quickshop/fileportlek/old/YAMLFile.java
@@ -1,55 +1,55 @@
-/*
- * This file is a part of project QuickShop, the name is YAMLFile.java
- *  Copyright (C) PotatoCraft Studio and contributors
- *
- *  This program is free software: you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the
- *  Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
-package org.maxgamer.quickshop.fileportlek.old;
-
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.InputStreamReader;
-
-public class YAMLFile extends FileEnvelope {
-
-    public YAMLFile(@NotNull final Plugin plugin, @NotNull final File file, @NotNull final String resourcePath, boolean loadDefault) {
-        super(plugin, file, resourcePath.endsWith(".yml") ? resourcePath : resourcePath + ".yml", loadDefault);
-    }
-
-    public YAMLFile(@NotNull final Plugin plugin, @NotNull final String fileName) {
-        this(plugin, "", fileName);
-    }
-
-    public YAMLFile(@NotNull final Plugin plugin, @NotNull final String resourcePath, @NotNull final String fileName) {
-        this(plugin, new File(plugin.getDataFolder().getAbsolutePath() + (resourcePath.startsWith(File.separator) ? resourcePath : File.separator + resourcePath), fileName.endsWith(".yml") ? fileName : fileName + ".yml"), resourcePath.isEmpty() ? fileName : resourcePath.endsWith(File.separator) ? resourcePath + fileName : resourcePath + File.separator + fileName);
-    }
-
-    public YAMLFile(@NotNull final Plugin plugin, @NotNull final File file, @NotNull final String resourcePath) {
-        super(plugin, file, resourcePath.endsWith(".yml") ? resourcePath : resourcePath + ".yml", true);
-    }
-
-    @Override
-    public void reload() {
-        fileConfiguration = YamlConfiguration.loadConfiguration(file);
-        if (loadDefault) {
-            fileConfiguration.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(getInputStream())));
-        }
-    }
-
-}
+///*
+// * This file is a part of project QuickShop, the name is YAMLFile.java
+// *  Copyright (C) PotatoCraft Studio and contributors
+// *
+// *  This program is free software: you can redistribute it and/or modify it
+// *  under the terms of the GNU General Public License as published by the
+// *  Free Software Foundation, either version 3 of the License, or
+// *  (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful, but WITHOUT
+// *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// *  for more details.
+// *
+// *  You should have received a copy of the GNU General Public License
+// *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+// *
+// */
+//
+//package org.maxgamer.quickshop.fileportlek.old;
+//
+//import org.bukkit.configuration.file.YamlConfiguration;
+//import org.bukkit.plugin.Plugin;
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.io.File;
+//import java.io.InputStreamReader;
+//
+//public class YAMLFile extends FileEnvelope {
+//
+//    public YAMLFile(@NotNull final Plugin plugin, @NotNull final File file, @NotNull final String resourcePath, boolean loadDefault) {
+//        super(plugin, file, resourcePath.endsWith(".yml") ? resourcePath : resourcePath + ".yml", loadDefault);
+//    }
+//
+//    public YAMLFile(@NotNull final Plugin plugin, @NotNull final String fileName) {
+//        this(plugin, "", fileName);
+//    }
+//
+//    public YAMLFile(@NotNull final Plugin plugin, @NotNull final String resourcePath, @NotNull final String fileName) {
+//        this(plugin, new File(plugin.getDataFolder().getAbsolutePath() + (resourcePath.startsWith(File.separator) ? resourcePath : File.separator + resourcePath), fileName.endsWith(".yml") ? fileName : fileName + ".yml"), resourcePath.isEmpty() ? fileName : resourcePath.endsWith(File.separator) ? resourcePath + fileName : resourcePath + File.separator + fileName);
+//    }
+//
+//    public YAMLFile(@NotNull final Plugin plugin, @NotNull final File file, @NotNull final String resourcePath) {
+//        super(plugin, file, resourcePath.endsWith(".yml") ? resourcePath : resourcePath + ".yml", true);
+//    }
+//
+//    @Override
+//    public void reload() {
+//        fileConfiguration = YamlConfiguration.loadConfiguration(file);
+//        if (loadDefault) {
+//            fileConfiguration.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(getInputStream())));
+//        }
+//    }
+//
+//}
