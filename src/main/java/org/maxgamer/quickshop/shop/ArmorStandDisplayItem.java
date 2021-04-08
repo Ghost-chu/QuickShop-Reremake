0f235f25e16062821cc9ef4547c5b72baa94fc0f
Ghost_chu <2908803755@qq.com>
2021-04-08 16:35:09 +0800

    Fix build error

--- src/main/java/org/maxgamer/quickshop/shop/ArmorStandDisplayItem.java
+++ src/main/java/org/maxgamer/quickshop/shop/ArmorStandDisplayItem.java
@@ -1,23 +1,22 @@
-///*
-// * This file is a part of project QuickShop, the name is ArmorStandDisplayItem.java
-// *  Copyright (C) PotatoCraft Studio and contributors
-// *
-// *  This program is free software: you can redistribute it and/or modify it
-// *  under the terms of the GNU General Public License as published by the
-// *  Free Software Foundation, either version 3 of the License, or
-// *  (at your option) any later version.
-// *
-// *  This program is distributed in the hope that it will be useful, but WITHOUT
-// *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// *  for more details.
-// *
-// *  You should have received a copy of the GNU General Public License
-// *  along with this program. If not, see <http://www.gnu.org/licenses/>.
-// *
-// */
-//
-//package org.maxgamer.quickshop.shop;
+/*
+ * This file is a part of project QuickShop, the name is ArmorStandDisplayItem.java
+ *  Copyright (C) PotatoCraft Studio and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.maxgamer.quickshop.shop;
 //
 //import lombok.ToString;
 //import org.bukkit.Location;
