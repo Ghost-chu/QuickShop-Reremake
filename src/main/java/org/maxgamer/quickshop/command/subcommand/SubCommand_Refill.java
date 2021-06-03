ed47cd37b0b9814d35ac54c50e072a0c08ba1613
Chris <65610536+Chris6ix@users.noreply.github.com>
2021-06-03 19:53:53 +0200

    Quick message fix

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_Refill.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_Refill.java
@@ -44,7 +44,7 @@ public class SubCommand_Refill implements CommandProcesser {
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (!(sender instanceof Player)) {
-            MsgUtil.sendMessage(sender, "This command can't be run by the console");
+            MsgUtil.sendMessage(sender, "This command can't be run by the console!");
             return;
         }
 
