71f7d1b0a5413a7679921d9c51d0ae1cdba72f41
Chris <65610536+Chris6ix@users.noreply.github.com>
2021-06-03 18:20:37 +0200

    Small correction in SubCommand_SuperCreate.java

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SuperCreate.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SuperCreate.java
@@ -47,7 +47,7 @@ public class SubCommand_SuperCreate implements CommandProcesser {
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (!(sender instanceof Player)) {
-            MsgUtil.sendMessage(sender, "This command can't be run by console");
+            MsgUtil.sendMessage(sender, "This command can't be run by the console!");
             return;
         }
 
