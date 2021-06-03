d2359d6304f3405504c278a5f5b1551df2f30e5f
Chris <65610536+Chris6ix@users.noreply.github.com>
2021-06-03 18:08:54 +0200

    Small correction in SubCommand_SilentBuy.java

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentBuy.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_SilentBuy.java
@@ -41,7 +41,7 @@ public class SubCommand_SilentBuy implements CommandProcesser {
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (cmdArg.length < 1) {
-            Util.debugLog("Exception on command, cancel.");
+            Util.debugLog("Exception on command! Cancelling!");
             return;
         }
 
