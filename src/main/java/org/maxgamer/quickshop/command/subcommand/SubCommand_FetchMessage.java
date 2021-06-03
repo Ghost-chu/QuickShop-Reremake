7735a148c38ba22aa675d0de18d4d4aa8d932ac7
Chris <65610536+Chris6ix@users.noreply.github.com>
2021-06-03 19:51:23 +0200

    Quick message improvement

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_FetchMessage.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_FetchMessage.java
@@ -39,7 +39,7 @@ public class SubCommand_FetchMessage implements CommandProcesser {
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (!(sender instanceof Player)) {
-            MsgUtil.sendMessage(sender, ChatColor.RED + "Only players can use this command!");
+            MsgUtil.sendMessage(sender, ChatColor.RED + "This command can't be run by the console!");
             return;
         }
 
