c08276836db93331192b0ecd3abe093c88df961e
Ghost_chu <2908803755@qq.com>
2021-06-11 19:30:04 +0800

    Forgot change it back

--- src/main/java/org/maxgamer/quickshop/shop/InventoryPreview.java
+++ src/main/java/org/maxgamer/quickshop/shop/InventoryPreview.java
@@ -111,7 +111,7 @@ public class InventoryPreview implements Listener {
         }
         final int size = 9;
 
-        inventory = plugin.getServer().createInventory(new QuickShopPreviewInventoryHolder(), 9, MsgUtil.getMessage("menu.preview", player));
+        inventory = plugin.getServer().createInventory(new QuickShopPreviewInventoryHolder(), size, MsgUtil.getMessage("menu.preview", player));
         for (int i = 0; i < size; i++) {
             inventory.setItem(i, itemStack);
         }
