1e38caaf81b9d97039eb2f747a13c6b62ffa206b
Ghost_chu <2908803755@qq.com>
2021-06-11 17:21:45 +0800

    Fix column numbers

--- src/main/java/org/maxgamer/quickshop/shop/InventoryPreview.java
+++ src/main/java/org/maxgamer/quickshop/shop/InventoryPreview.java
@@ -111,7 +111,7 @@ public class InventoryPreview implements Listener {
         }
         final int size = 9;
 
-        inventory = plugin.getServer().createInventory(new QuickShopPreviewInventoryHolder(), 0, MsgUtil.getMessage("menu.preview", player));
+        inventory = plugin.getServer().createInventory(new QuickShopPreviewInventoryHolder(), 9, MsgUtil.getMessage("menu.preview", player));
         for (int i = 0; i < size; i++) {
             inventory.setItem(i, itemStack);
         }
