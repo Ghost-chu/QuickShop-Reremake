dd98bc9bfb7ec0bf015f66959f5ea657316960ef
Ghost_chu <2908803755@qq.com>
2021-06-15 13:39:23 +0800

    Add @Override

--- src/main/java/org/maxgamer/quickshop/command/processor/CommonCommandProcessor.java
+++ src/main/java/org/maxgamer/quickshop/command/processor/CommonCommandProcessor.java
@@ -1,3 +1,22 @@
+/*
+ * This file is a part of project QuickShop, the name is CommonCommandProcessor.java
+ *  Copyright (C) PotatoCraft Studio and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.maxgamer.quickshop.command.processor;
 
 import org.bukkit.command.CommandSender;
@@ -20,6 +39,7 @@ public abstract class CommonCommandProcessor implements ICommandProcessor {
      * @param cmdArg       Args
      * @param commandLabel The command prefix /qs is qs
      */
+    @Override
     public void onCommand(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
 
@@ -38,6 +58,7 @@ public abstract class CommonCommandProcessor implements ICommandProcessor {
      * @param commandLabel The command prefix /qs is qs
      * @return The result for tab-complete lists
      */
+    @Override
     @Nullable
     public List<String> onTabComplete(
             @NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
