481b28931e656c258eb8451acc0125efaada6096
Ghost_chu <2908803755@qq.com>
2021-06-21 10:57:32 +0800

    Also cancel tasks when we setup boot error

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -1880,6 +1880,7 @@ public class QuickShop extends JavaPlugin {
     public void setupBootError(BootError bootError) {
         this.bootError = bootError;
         HandlerList.unregisterAll(this);
+        Bukkit.getScheduler().cancelTasks(this);
     }
 
     public void registerCustomCommands() {
