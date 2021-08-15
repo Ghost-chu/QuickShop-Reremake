be87a06fab8937c433ea60505b156f874cdfe733
sandtechnology <a1294790523@hotmail.com>
2021-08-15 19:27:40 +0800

    Fix Test

--- src/test/java/org/maxgamer/quickshop/eventmanager/ListenerContainerTest.java
+++ src/test/java/org/maxgamer/quickshop/eventmanager/ListenerContainerTest.java
@@ -20,17 +20,145 @@
 package org.maxgamer.quickshop.eventmanager;
 
 
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginBase;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class ListenerContainerTest {
 
     @Test
     public void testMatches() {
-        Plugin testPlugin = new JavaPlugin() {
+        Plugin testPlugin = new PluginBase() {
+            @NotNull
+            @Override
+            public File getDataFolder() {
+                return null;
+            }
 
+            @NotNull
+            @Override
+            public PluginDescriptionFile getDescription() {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public FileConfiguration getConfig() {
+                return null;
+            }
+
+            @Nullable
+            @Override
+            public InputStream getResource(@NotNull String s) {
+                return null;
+            }
+
+            @Override
+            public void saveConfig() {
+
+            }
+
+            @Override
+            public void saveDefaultConfig() {
+
+            }
+
+            @Override
+            public void saveResource(@NotNull String s, boolean b) {
+
+            }
+
+            @Override
+            public void reloadConfig() {
+
+            }
+
+            @NotNull
+            @Override
+            public PluginLoader getPluginLoader() {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public Server getServer() {
+                return null;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+
+            @Override
+            public void onDisable() {
+
+            }
+
+            @Override
+            public void onLoad() {
+
+            }
+
+            @Override
+            public void onEnable() {
+
+            }
+
+            @Override
+            public boolean isNaggable() {
+                return false;
+            }
+
+            @Override
+            public void setNaggable(boolean b) {
+
+            }
+
+            @Nullable
+            @Override
+            public ChunkGenerator getDefaultWorldGenerator(@NotNull String s, @Nullable String s1) {
+                return null;
+            }
+
+            @Nullable
+            @Override
+            public BiomeProvider getDefaultBiomeProvider(@NotNull String s, @Nullable String s1) {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public Logger getLogger() {
+                return null;
+            }
+
+            @Override
+            public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+                return false;
+            }
+
+            @Nullable
+            @Override
+            public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+                return null;
+            }
         };
         ListenerContainer listenerContainerA = new ListenerContainer(null, "@QuickTest");
         ListenerContainer listenerContainerB = new ListenerContainer(null, "@QuickTestBad");
