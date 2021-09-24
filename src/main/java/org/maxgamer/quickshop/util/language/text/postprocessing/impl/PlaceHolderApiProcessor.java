35bbc9803507222c5045276892e061d68aaab18d
Ghost_chu <2908803755@qq.com>
2021-09-24 21:33:03 +0800

    revert all changes about bump language level, adapt PAPIP with Java8

--- src/main/java/org/maxgamer/quickshop/util/language/text/postprocessing/impl/PlaceHolderApiProcessor.java
+++ src/main/java/org/maxgamer/quickshop/util/language/text/postprocessing/impl/PlaceHolderApiProcessor.java
@@ -11,9 +11,9 @@ import org.maxgamer.quickshop.util.language.text.postprocessing.PostProcessor;
 public class PlaceHolderApiProcessor implements PostProcessor {
     @Override
     public @NotNull String process(@NotNull String text, @Nullable CommandSender sender, String... args) {
-        if(sender instanceof OfflinePlayer player) {
+        if(sender instanceof OfflinePlayer) {
             if (Bukkit.getPluginManager().isPluginEnabled("PlaceHolderAPI")) {
-                return PlaceholderAPI.setPlaceholders(player,text);
+                return PlaceholderAPI.setPlaceholders((OfflinePlayer) sender,text);
             }
         }
         return text;
