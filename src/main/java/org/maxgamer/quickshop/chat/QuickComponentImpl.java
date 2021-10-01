9b6f159bc94768482517a186d73afc4437253e45
Ghost_chu <2908803755@qq.com>
2021-10-01 13:52:45 +0800

    revert adventure changes

--- src/main/java/org/maxgamer/quickshop/chat/QuickComponentImpl.java
+++ src/main/java/org/maxgamer/quickshop/chat/QuickComponentImpl.java
@@ -1,42 +1,42 @@
-///*
-// * This file is a part of project QuickShop, the name is QuickComponentImpl.java
-// *  Copyright (C) PotatoCraft Studio and contributors
-// *
-// *  This program is free software: you can redistribute it and/or modify it
-// *  under the terms of the GNU General Public License as published by the
-// *  Free Software Foundation, either version 3 of the License, or
-// *  (at your option) any later version.
-// *
-// *  This program is distributed in the hope that it will be useful, but WITHOUT
-// *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// *  for more details.
-// *
-// *  You should have received a copy of the GNU General Public License
-// *  along with this program. If not, see <http://www.gnu.org/licenses/>.
-// *
-// */
-//
-//package org.maxgamer.quickshop.chat;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import org.jetbrains.annotations.NotNull;
-//
-//@AllArgsConstructor
-//@Builder
-//@Data
-//public class QuickComponentImpl implements QuickComponent {
-//    private Object component;
-//
-//    @Override
-//    public @NotNull Object get() {
-//        return this.component;
-//    }
-//
-//    @Override
-//    public void set(@NotNull Object component) {
-//        this.component = component;
-//    }
-//}
+/*
+ * This file is a part of project QuickShop, the name is QuickComponentImpl.java
+ *  Copyright (C) PotatoCraft Studio and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.maxgamer.quickshop.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class QuickComponentImpl implements QuickComponent {
+    private Object component;
+
+    @Override
+    public @NotNull Object get() {
+        return this.component;
+    }
+
+    @Override
+    public void set(@NotNull Object component) {
+        this.component = component;
+    }
+}
