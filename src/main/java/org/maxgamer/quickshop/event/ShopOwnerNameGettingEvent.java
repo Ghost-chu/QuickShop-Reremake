e5c83b6e33901cef62e0969a9c8d65321a562954
Ghost_chu <2908803755@qq.com>
2021-10-03 19:29:07 +0800

    #1510 Extend ability to allow user change the owner name display in chat and sign

---
+++ src/main/java/org/maxgamer/quickshop/event/ShopOwnerNameGettingEvent.java
@@ -0,0 +1,14 @@
+package org.maxgamer.quickshop.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.maxgamer.quickshop.shop.Shop;
+
+import java.util.UUID;
+@AllArgsConstructor
+@Data
+public class ShopOwnerNameGettingEvent extends AbstractQSEvent{
+    private Shop shop;
+    private UUID owner;
+    private String name;
+}
