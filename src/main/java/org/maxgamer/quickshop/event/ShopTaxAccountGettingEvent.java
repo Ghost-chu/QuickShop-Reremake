ba25fba010b9b267d15a73b8f70fa965e4580198
Ghost_chu <2908803755@qq.com>
2021-10-04 10:58:27 +0800

    Allow addon provide custom taxAccount

--- src/main/java/org/maxgamer/quickshop/event/ShopTaxAccountGettingEvent.java
+++ src/main/java/org/maxgamer/quickshop/event/ShopTaxAccountGettingEvent.java
@@ -3,15 +3,15 @@ package org.maxgamer.quickshop.event;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.shop.Shop;
 
 import java.util.UUID;
-
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @Data
-public class ShopOwnerNameGettingEvent extends AbstractQSEvent {
-    private Shop shop;
-    private UUID owner;
-    private String name;
+public class ShopTaxAccountGettingEvent extends AbstractQSEvent{
+    @Nullable
+    private UUID taxAccount;
+    private final Shop shop;
 }
