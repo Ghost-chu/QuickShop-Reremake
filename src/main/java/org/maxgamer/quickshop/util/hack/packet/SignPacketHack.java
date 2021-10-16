211b201285b614d222c7bc2672c540bc7245c7fc
2908803755@qq.com <wyf001912>
2021-10-16 15:53:25 +0800

    Temp remove PacketHack util, we don't add that feature in v5.0

--- src/main/java/org/maxgamer/quickshop/util/hack/packet/SignPacketHack.java
+++ src/main/java/org/maxgamer/quickshop/util/hack/packet/SignPacketHack.java
@@ -1,59 +1,59 @@
-package org.maxgamer.quickshop.util.hack.packet;
-
-import com.bergerkiller.bukkit.common.nbt.CommonTagCompound;
-import com.bergerkiller.bukkit.common.protocol.CommonPacket;
-import com.bergerkiller.bukkit.common.protocol.PacketType;
-import com.bergerkiller.bukkit.common.utils.BlockUtil;
-import com.bergerkiller.bukkit.common.utils.PacketUtil;
-import net.md_5.bungee.chat.ComponentSerializer;
-import org.apache.commons.lang.Validate;
-import org.bukkit.Bukkit;
-import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.maxgamer.quickshop.QuickShop;
-import org.maxgamer.quickshop.api.chat.ComponentPackage;
-
-import java.util.List;
-
-/**
- * Little treat, we modify the sign component to translatable component if possible...
- */
-public class SignPacketHack {
-
-    private final QuickShop plugin;
-
-    public SignPacketHack(QuickShop plugin) {
-        this.plugin = plugin;
-        Validate.isTrue(Bukkit.getPluginManager().isPluginEnabled("BKCommonLib"), "Require BKCommonLib installed!");
-    }
-
-    public void sendLines(@NotNull Sign sign, @NotNull List<ComponentPackage> lines, Player player) {
-        if (player != null) {
-            CommonPacket updatePacket = BlockUtil.getUpdatePacket(sign);
-            if (updatePacket != null) {
-                applyToSign(updatePacket, lines);
-                PacketUtil.sendPacket(player, updatePacket);
-            }
-        }
-    }
-
-    public void applyToSign(@NotNull CommonPacket updatePacket, @NotNull List<ComponentPackage> lines) {
-        if (updatePacket.getType() == PacketType.OUT_TILE_ENTITY_DATA) {
-            // >= MC 1.10.2
-
-            CommonTagCompound compound = updatePacket.read(PacketType.OUT_TILE_ENTITY_DATA.data);
-            // ====================================================================
-
-            for (int i = 0; i < lines.size(); i++) {
-                String key = "Text" + (i + 1);
-                String text = ComponentSerializer.toString(lines.get(i).getComponents());
-                compound.putValue(key, text);
-            }
-
-            // ====================================================================
-            updatePacket.write(PacketType.OUT_TILE_ENTITY_DATA.data, compound);
-        }
-    }
-
-}
+//package org.maxgamer.quickshop.util.hack.packet;
+//
+//import com.bergerkiller.bukkit.common.nbt.CommonTagCompound;
+//import com.bergerkiller.bukkit.common.protocol.CommonPacket;
+//import com.bergerkiller.bukkit.common.protocol.PacketType;
+//import com.bergerkiller.bukkit.common.utils.BlockUtil;
+//import com.bergerkiller.bukkit.common.utils.PacketUtil;
+//import net.md_5.bungee.chat.ComponentSerializer;
+//import org.apache.commons.lang.Validate;
+//import org.bukkit.Bukkit;
+//import org.bukkit.block.Sign;
+//import org.bukkit.entity.Player;
+//import org.jetbrains.annotations.NotNull;
+//import org.maxgamer.quickshop.QuickShop;
+//import org.maxgamer.quickshop.api.chat.ComponentPackage;
+//
+//import java.util.List;
+//
+///**
+// * Little treat, we modify the sign component to translatable component if possible...
+// */
+//public class SignPacketHack {
+//
+//    private final QuickShop plugin;
+//
+//    public SignPacketHack(QuickShop plugin) {
+//        this.plugin = plugin;
+//        Validate.isTrue(Bukkit.getPluginManager().isPluginEnabled("BKCommonLib"), "Require BKCommonLib installed!");
+//    }
+//
+//    public void sendLines(@NotNull Sign sign, @NotNull List<ComponentPackage> lines, Player player) {
+//        if (player != null) {
+//            CommonPacket updatePacket = BlockUtil.getUpdatePacket(sign);
+//            if (updatePacket != null) {
+//                applyToSign(updatePacket, lines);
+//                PacketUtil.sendPacket(player, updatePacket);
+//            }
+//        }
+//    }
+//
+//    public void applyToSign(@NotNull CommonPacket updatePacket, @NotNull List<ComponentPackage> lines) {
+//        if (updatePacket.getType() == PacketType.OUT_TILE_ENTITY_DATA) {
+//            // >= MC 1.10.2
+//
+//            CommonTagCompound compound = updatePacket.read(PacketType.OUT_TILE_ENTITY_DATA.data);
+//            // ====================================================================
+//
+//            for (int i = 0; i < lines.size(); i++) {
+//                String key = "Text" + (i + 1);
+//                String text = ComponentSerializer.toString(lines.get(i).getComponents());
+//                compound.putValue(key, text);
+//            }
+//
+//            // ====================================================================
+//            updatePacket.write(PacketType.OUT_TILE_ENTITY_DATA.data, compound);
+//        }
+//    }
+//
+//}
